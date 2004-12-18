e130b3fa83982bedfd8a9f7ab2a2be7d58391e6c
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2004-12-18 14:49:32 +0000

    SVN test changed  (this is test commit)

--- javasvn-test/src/org/tmatesoft/svn/core/test/SVN.java
+++ javasvn-test/src/org/tmatesoft/svn/core/test/SVN.java
@@ -12,20 +12,18 @@
 
 package org.tmatesoft.svn.core.test;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 
-import org.tmatesoft.svn.core.ISVNStatusHandler;
-import org.tmatesoft.svn.core.ISVNWorkspace;
-import org.tmatesoft.svn.core.SVNStatus;
-import org.tmatesoft.svn.core.SVNWorkspaceManager;
+import org.tmatesoft.svn.core.diff.SVNDiffWindow;
 import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
 import org.tmatesoft.svn.core.internal.ws.fs.FSEntryFactory;
-import org.tmatesoft.svn.core.internal.ws.fs.FSUtil;
-import org.tmatesoft.svn.core.io.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.io.ISVNEditor;
+import org.tmatesoft.svn.core.io.ISVNReporter;
+import org.tmatesoft.svn.core.io.ISVNReporterBaton;
+import org.tmatesoft.svn.core.io.SVNCommitInfo;
 import org.tmatesoft.svn.core.io.SVNException;
-import org.tmatesoft.svn.core.io.SVNLogEntry;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.io.SVNRepositoryLocation;
@@ -40,91 +38,97 @@ public class SVN {
         DAVRepositoryFactory.setup();
         SVNRepositoryFactoryImpl.setup();
         FSEntryFactory.setup();
-        if (args == null || args.length < 3) {
-            System.err.println("USAGE SVN EXPORT | CHECKOUT URL PATH PATH2");
-            System.exit(1);
-        }
-        try {
-            SVNRepository repos = SVNRepositoryFactory.create(SVNRepositoryLocation.parseURL(args[1].trim()));
-            repos.log(new String[] {"bin"}, 50000, -1, false, false, new ISVNLogEntryHandler() {
-               public void handleLogEntry(SVNLogEntry logEntry) {
-                   System.err.println(logEntry.getAuthor() + ":" + logEntry.getRevision() + "\n" + logEntry.getMessage());
-               } 
-            });
-        } catch (SVNException e) {
-            e.printStackTrace();
-        }
-        System.exit(0);
         
-        long start = System.currentTimeMillis();
-        boolean export = "export".equalsIgnoreCase(args[0].trim());
-        File path = new File(args[2]);
-        ISVNWorkspace ws = null;
+        String url = "http://72.9.228.230:8080/svn/jsvn/trunk/";
         try {
-            FSUtil.deleteAll(path);
-            path.mkdirs();
-            start = System.currentTimeMillis();
-            SVNRepositoryLocation url = SVNRepositoryLocation.parseURL(args[1]);
-            ws = SVNWorkspaceManager.createWorkspace("file", path.getAbsolutePath());
-            ws.checkout(url, ISVNWorkspace.HEAD, export);
-        } catch (SVNException e) {
-            System.err.println("error: " + e.getMessage());
-        }
-        
-        long time = (System.currentTimeMillis() - start);
-        DebugLog.benchmark("checkout took:  " + time + " ms.");
-        if (args.length == 4) {
-            path = new File(args[3]);
-            AllTests.deleteAll(path);
-            path.mkdirs();
-            long time2 = System.currentTimeMillis();
-            try {
-                runSVNAnonCommand("co", new String[] {args[1], path.getAbsolutePath()});
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            time2 = System.currentTimeMillis() - time2;
-            DebugLog.benchmark("NATIVE CHECKOUT: " + time2 + " ms.");
-            DebugLog.benchmark("RATIO: " + ((double) time)/((double) time2));
-        }
-        try {
-            ws.status("", false, new ISVNStatusHandler() {
-                public void handleStatus(String p, SVNStatus status) {
-                    DebugLog.benchmark(status.getContentsStatus() + ": " + p);
+            SVNRepositoryLocation location = SVNRepositoryLocation.parseURL(url);
+            SVNRepository repository = SVNRepositoryFactory.create(location);
+            
+            ISVNReporterBaton reporterBaton = new ISVNReporterBaton() {
+                public void report(ISVNReporter reporter) throws SVNException {
+                    reporter.setPath("", 21, false);
+                    reporter.finishReport();
                 }
-            }, true, false, false);
-        } catch (SVNException e) {
-            e.printStackTrace();
-        }
-        System.exit(0);
-    }
+            };
+            
+            ISVNEditor editor = new ISVNEditor() {
 
-    public static void runSVNAnonCommand(String command, String[] params) throws IOException {
-        String[] realCommand = new String[params.length + 2];
-        realCommand[0] = "svn";
-        realCommand[1] = command;        
-        for(int i = 0; i < params.length; i++) {
-            realCommand[i + 2] = params[i];
-        }
-        execCommand(realCommand, true);            
-    }
-
-    private static Process execCommand(String[] command, boolean wait) throws IOException {
-        Process process = Runtime.getRuntime().exec(command);
-        if (process != null) {
-            try {
-                new AllTests.ReaderThread(process.getInputStream()).start();
-                new AllTests.ReaderThread(process.getErrorStream()).start();
-                if (wait) {
-                    int code = process.waitFor();
-                    if (code != 0) {
-                        throw new IOException("process '"  +  command[0] + "' exit code is not 0 : " + code);
-                    }
+                public void targetRevision(long revision) throws SVNException {
                 }
-            } catch (InterruptedException e) {
-                throw new IOException("interrupted");
-            }
+
+                public void openRoot(long revision) throws SVNException {
+                    DebugLog.log("OPEN ROOT: " + revision);
+                }
+
+                public void deleteEntry(String path, long revision) throws SVNException {
+                    DebugLog.log("DELETE");
+                }
+
+                public void absentDir(String path) throws SVNException {
+                }
+
+                public void absentFile(String path) throws SVNException {
+                }
+
+                public void addDir(String path, String copyFromPath, long copyFromRevision) throws SVNException {
+                    DebugLog.log("ADD DIR");
+                }
+
+                public void openDir(String path, long revision) throws SVNException {
+                    DebugLog.log("OPEN DIR");
+                }
+
+                public void changeDirProperty(String name, String value) throws SVNException {
+                    DebugLog.log("CHANGE DIR PROPERTY: " + name + "=" + value);
+                }
+
+                public void closeDir() throws SVNException {
+                    DebugLog.log("CLOSE DIR");
+                }
+
+                public void addFile(String path, String copyFromPath, long copyFromRevision) throws SVNException {
+                    DebugLog.log("ADD FILE");
+                }
+
+                public void openFile(String path, long revision) throws SVNException {
+                    DebugLog.log("OPEN FILE: " + path);
+                }
+
+                public void applyTextDelta(String baseChecksum) throws SVNException {
+                    DebugLog.log("APPLY DELTA");
+                }
+
+                public void changeFileProperty(String name, String value) throws SVNException {
+                    DebugLog.log("CHANGE FILE PROPERTY: " + name + "=" + value);
+                }
+
+                public void closeFile(String textChecksum) throws SVNException {
+                    DebugLog.log("CLOSE FILE");
+                }
+
+                public SVNCommitInfo closeEdit() throws SVNException {
+                    DebugLog.log("CLOSE EDIT");
+                    return null;
+                }
+
+                public void abortEdit() throws SVNException {
+                    DebugLog.log("ABORT EDIT");
+                }
+
+                public OutputStream textDeltaChunk(SVNDiffWindow diffWindow) throws SVNException {
+                    DebugLog.log("RECORDING DELTA CHUNK ");
+                    return new ByteArrayOutputStream();
+                }
+
+                public void textDeltaEnd() throws SVNException {
+                }
+                
+            };
+            
+            String targetURL = url + "changelog.txt";
+            repository.diff(targetURL, 23, "changelog.txt", false, false, reporterBaton, editor);
+        } catch (SVNException e) {
+            DebugLog.error(e);
         }
-        return process;
     }
 }
