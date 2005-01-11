7054968e029cdc3c14c9fcaf74931f2a9251e78a
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2005-01-11 15:05:43 +0000

    commented out debug option added

--- javasvn/cli/svn.bat
+++ javasvn/cli/svn.bat
@@ -6,5 +6,6 @@ if "%JAVASVN_HOME%"=="" set JAVASVN_HOME=%DEFAULT_JAVASVN_HOME%
 
 set JAVASVN_CLASSPATH= "%JAVASVN_HOME%javasvn.jar";"%JAVASVN_HOME%javasvn-cli.jar";"%JAVASVN_HOME%jsch-0.1.18.jar"
 set JAVASVN_MAINCLASS=org.tmatesoft.svn.cli.SVN
+REM set JAVASVN_OPTIONS=-Djavasvn.log.svn=true
 
 "%JAVA_HOME%\bin\java" %JAVASVN_OPTIONS% -cp %JAVASVN_CLASSPATH% %JAVASVN_MAINCLASS% %*
