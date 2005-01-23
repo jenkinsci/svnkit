54f1ef28c1134cbab9992074956ddbd55f4028fa
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2005-01-23 17:28:59 +0000

    - jsch is updated to version 0.1.19 - jsch library now named jsch.jar - build.xml refactored and cleaned up - license files renamed for consitency

--- javasvn/cli/svn.bat
+++ javasvn/cli/svn.bat
@@ -4,7 +4,7 @@ set DEFAULT_JAVASVN_HOME=%~dp0
 
 if "%JAVASVN_HOME%"=="" set JAVASVN_HOME=%DEFAULT_JAVASVN_HOME%
 
-set JAVASVN_CLASSPATH= "%JAVASVN_HOME%javasvn.jar";"%JAVASVN_HOME%javasvn-cli.jar";"%JAVASVN_HOME%jsch-0.1.18.jar"
+set JAVASVN_CLASSPATH= "%JAVASVN_HOME%javasvn.jar";"%JAVASVN_HOME%javasvn-cli.jar";"%JAVASVN_HOME%jsch.jar"
 set JAVASVN_MAINCLASS=org.tmatesoft.svn.cli.SVN
 REM set JAVASVN_OPTIONS=-Djavasvn.log.svn=true
 
