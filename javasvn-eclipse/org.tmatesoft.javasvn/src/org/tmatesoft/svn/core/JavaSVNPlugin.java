e5211277a5d88d51f00c347d9f54a81eb00a50c9
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2005-02-03 18:05:37 +0000

    eclipse plugin building refactoring: - update site is added - automated subclipse "patch" feature versioning schema changed, version numbers are used instead of dates

---
+++ javasvn-eclipse/org.tmatesoft.javasvn/src/org/tmatesoft/svn/core/JavaSVNPlugin.java
@@ -0,0 +1,35 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://tmate.org/svn/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.core;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
+import org.tmatesoft.svn.core.internal.ws.fs.FSEntryFactory;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class JavaSVNPlugin extends Plugin {
+
+    public JavaSVNPlugin() {
+    }
+
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        DAVRepositoryFactory.setup();
+        SVNRepositoryFactoryImpl.setup();
+         
+        FSEntryFactory.setup();
+    }
+}
