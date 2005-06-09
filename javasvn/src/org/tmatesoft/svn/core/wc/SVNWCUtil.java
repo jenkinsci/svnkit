31d7b375ef58cdad32227620f742e981e3228558
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2005-06-09 11:14:40 +0000

    utility method to get wc root added

--- javasvn/src/org/tmatesoft/svn/core/wc/SVNWCUtil.java
+++ javasvn/src/org/tmatesoft/svn/core/wc/SVNWCUtil.java
@@ -14,6 +14,8 @@ import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
 import org.tmatesoft.svn.core.internal.wc.SVNTranslator;
 import org.tmatesoft.svn.core.internal.wc.SVNWCAccess;
+import org.tmatesoft.svn.core.internal.wc.SVNProperties;
+import org.tmatesoft.svn.core.internal.wc.SVNExternalInfo;
 import org.tmatesoft.svn.core.io.SVNException;
 
 public class SVNWCUtil {
@@ -104,5 +106,84 @@ public class SVNWCUtil {
 
     public static boolean isBinaryMimetype(String mimetype) {
         return mimetype != null && !mimetype.startsWith("text/");
+    }
+
+    public static boolean isWorkingCopyRoot(final File versionedDir) {
+        if (versionedDir == null || !SVNWCAccess.isVersionedDirectory(versionedDir)) {
+            // unversioned.
+            return false;
+        }
+        // call status of parent instead:
+        if (versionedDir.getParentFile() == null) {
+            return true;
+        }
+        SVNStatusClient stClient = new SVNStatusClient(null, null);
+        final boolean[] isRoot = new boolean[] {false};
+        try {
+            stClient.doStatus(versionedDir.getParentFile(), false, false, true, true, new ISVNStatusHandler() {
+                public void handleStatus(SVNStatus status) {
+                    if (!isRoot[0] && versionedDir.equals(status.getFile())) {
+                        if (status.getContentsStatus() == SVNStatusType.STATUS_IGNORED ||
+                                status.getContentsStatus() == SVNStatusType.STATUS_UNVERSIONED ||
+                                status.getContentsStatus() == SVNStatusType.STATUS_EXTERNAL) {
+                            isRoot[0] = true;
+                        }
+                    }
+                    // if dir is not versioned in parent or external -> return true
+                }
+            });
+        } catch (SVNException e) {
+            return true;
+        }
+        return isRoot[0];
+    }
+
+    public static File getWorkingCopyRoot(File versionedDir, boolean stopOnExtenrals) {
+        if (versionedDir == null || !SVNWCAccess.isVersionedDirectory(versionedDir)) {
+            // unversioned.
+            return null;
+        }
+
+        if (isWorkingCopyRoot(versionedDir)) {
+            // this is root.
+            if (stopOnExtenrals) {
+                return versionedDir;
+            }
+            File parent = versionedDir.getParentFile();
+            File parentRoot = getWorkingCopyRoot(parent, stopOnExtenrals);
+            if (parentRoot == null) {
+                // if parent is not versioned return this dir.
+                return versionedDir;
+            }
+            // parent is versioned. we have to check if it contains externals definition for this dir.
+
+            while(parent != null) {
+                try {
+                    SVNWCAccess parentAccess = SVNWCAccess.create(parent);
+                    SVNProperties props = parentAccess.getTarget().getProperties("", false);
+                    SVNExternalInfo[] externals = SVNWCAccess.parseExternals("", props.getPropertyValue(SVNProperty.EXTERNALS));
+                    // now externals could point to our dir.
+                    for (int i = 0; i < externals.length; i++) {
+                        SVNExternalInfo external = externals[i];
+                        File externalFile = new File(parent, external.getPath());
+                        if (externalFile.equals(versionedDir)) {
+                            return parentRoot;
+                        }
+                    }
+                } catch (SVNException e) {
+                }
+                if (parent.equals(parentRoot)) {
+                    break;
+                }
+                parent = parent.getParentFile();
+            }
+            return versionedDir;
+        } else {
+            // if dir is not a root -> just recurse till root, the call get root again.
+            if (versionedDir.getParentFile() != null) {
+                return getWorkingCopyRoot(versionedDir.getParentFile(), stopOnExtenrals);
+            }
+            return versionedDir;
+        }
     }
 }
