f8504f36242a53287c7c7fb6faa97b90de6e714c
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2005-06-18 14:18:24 +0000

    bug in cached credentials provider fixed: it was not possible to run the same test twice

--- javasvn/src/org/tmatesoft/svn/cli/SVNCommandLineCredentialsProvider.java
+++ javasvn/src/org/tmatesoft/svn/cli/SVNCommandLineCredentialsProvider.java
@@ -16,17 +16,19 @@ import java.util.Map;
 
 import org.tmatesoft.svn.core.io.ISVNCredentials;
 import org.tmatesoft.svn.core.io.ISVNCredentialsProvider;
+import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
+import org.tmatesoft.svn.util.DebugLog;
 
 public class SVNCommandLineCredentialsProvider implements
         ISVNCredentialsProvider {
-    
+
     private Credentials myCurrentCredentials;
     private Credentials myOriginalCredentials;
     private String myRealm;
     private Map myRealmIndexes = new HashMap();
 
     private static final char[] HEXADECIMAL = {
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
         'e', 'f'
     };
     private boolean myIsStoreCreds;
@@ -35,10 +37,10 @@ public class SVNCommandLineCredentialsProvider implements
         if (userName != null && password != null) {
             myCurrentCredentials = new Credentials(userName, password);
             myOriginalCredentials = myCurrentCredentials;
-        } 
+        }
         myIsStoreCreds = store;
     }
-    
+
     public ISVNCredentials nextCredentials(String realm) {
         myRealm = realm == null ? "default" : realm;
         if (myCurrentCredentials == null) {
@@ -47,7 +49,7 @@ public class SVNCommandLineCredentialsProvider implements
                 index = new Integer(0);
             } else {
                 index = new Integer(index.intValue() + 1);
-            }            
+            }
             Credentials creds = loadCredentials(myRealm, index.intValue());
             if (creds != null) {
                 myRealmIndexes.put(realm, index);
@@ -65,7 +67,7 @@ public class SVNCommandLineCredentialsProvider implements
         }
         // only if not there yet (!)
         int index = 0;
-        Credentials creds = null;
+        Credentials creds;
         do {
             creds = loadCredentials(myRealm, index);
             index++;
@@ -86,36 +88,42 @@ public class SVNCommandLineCredentialsProvider implements
         myRealmIndexes.clear();
         myRealm = null;
     }
-    
+
     private static Credentials loadCredentials(String realm, int index) {
         File file = getCrendentialsFile(realm, index);
         if (!file.exists()) {
             return null;
         }
+        DataInputStream dis = null;
         try {
-            DataInputStream dis = new DataInputStream(new FileInputStream(file));
+            dis = new DataInputStream(new FileInputStream(file));
             String name = dis.readUTF();
             String password = dis.readUTF();
-            dis.close();
             return new Credentials(name, password);
         } catch (IOException e) {
+            DebugLog.error(e);
+        } finally {
+            SVNFileUtil.closeFile(dis);
         }
         return null;
     }
-    
-    
+
+
     private static void saveCredentials(String realm, Credentials credentials) {
         File file = getCrendentialsFile(realm, -1);
         file.getParentFile().mkdirs();
+        DataOutputStream dos = null;
         try {
-            DataOutputStream dos = new DataOutputStream(new FileOutputStream(file));
+            dos = new DataOutputStream(new FileOutputStream(file));
             dos.writeUTF(credentials.getName());
             dos.writeUTF(credentials.getPassword());
-            dos.close();
         } catch (IOException e) {
+            DebugLog.error(e);
+        } finally {
+            SVNFileUtil.closeFile(dos);
         }
     }
-    
+
     private static File getCrendentialsFile(String realm, int index) {
         File dir = null;
         realm = realm == null ? "default" : realm;
@@ -124,12 +132,13 @@ public class SVNCommandLineCredentialsProvider implements
             String dirName = encode(hash);
             dir = new File(System.getProperty("user.home"), ".javasvn");
             dir = new File(dir, "auth-cache");
+            dir = new File(dir, dirName);
             if (index < 0) {
                 index = dir.list() == null ? 0 : dir.list().length;
             }
-            dir = new File(dir, dirName);
             dir = new File(dir, index + "");
         } catch (NoSuchAlgorithmException e) {
+            DebugLog.error(e);
         }
         return dir;
     }
@@ -137,24 +146,24 @@ public class SVNCommandLineCredentialsProvider implements
     private static String encode(byte[] binaryData) {
         if (binaryData.length != 16) {
             return null;
-        } 
+        }
 
         char[] buffer = new char[32];
         for (int i = 0; i < 16; i++) {
-            int low = (int) (binaryData[i] & 0x0f);
-            int high = (int) ((binaryData[i] & 0xf0) >> 4);
+            int low = binaryData[i] & 0x0f;
+            int high = (binaryData[i] & 0xf0) >> 4;
             buffer[i * 2] = HEXADECIMAL[high];
             buffer[(i * 2) + 1] = HEXADECIMAL[low];
         }
 
         return new String(buffer);
     }
-    
+
     private static class Credentials implements ISVNCredentials {
-        
+
         private String myUserName;
         private String myPassword;
-        
+
         public Credentials(String userName, String password) {
             myUserName = userName;
             myPassword = password;
@@ -166,6 +175,6 @@ public class SVNCommandLineCredentialsProvider implements
 
         public String getPassword() {
             return myPassword;
-        }        
+        }
     }
 }
