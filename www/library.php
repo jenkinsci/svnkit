f58995033d4745a25cac11004921c44a3a7c9855
sa <>
2005-07-30 19:31:49 +0000

    new site is added

---
+++ www/library.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head><title>Get&nbsp;JavaSVN&nbsp;Library</title>
+<meta name="keywords" content="Subversion,SVN,Version Control,Java,Library,Development,Team,Teamwork,Configuration Management,Software Configuration Management,SCM,CM,Revision Control,Collaboration,Open Source,Software Development,Collaborative Software Development">   
+<meta name="description" content="Pure Java Subversion Library. Open Source, provided by TMate Software">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" media="screen" href="home0000.css">
+
+
+<body>
+<table bgcolor="white" width="900" cellpadding="0" align="center" style="height: 100%;"  border="0" cellspacing="0" >
+<tr align="center" valign="top" >
+<td colspan="1" rowspan="1">
+
+<div class="leftedge" >
+<div class="rightedge">
+<!--<div style="width: 900px; background: url(bottombg.gif) repeat-x bottom; width: 99.5%;" >-->
+
+<table  cellpadding="0" align="center" width="870" style="height:100%" border="0" cellspacing="0">
+<tr align="center" valign="top">
+<td align="left" valign="top" style="padding: 20px 20px 20px 20px;">
+
+<p>
+<span class="javasvn"><a style="border-bottom: 0px; color: rgb(51, 102, 153);" href="http://tmate.org/svn/">JavaSVN</a>&nbsp;</span><span style="font-size: 140%; font-weight: bold;">The only pure Java Subversion client library in the world!</span>
+</p>
+<br>
+<p><div style="border-bottom: 1px solid #ccc;"></div></p>
+<small><a href="index.html" style="border-bottom: 0px;">Home</a></small>
+
+
+<h1>JavaSVN Library</h1>
+<!--Contents-->
+<br>
+<h4>Repository</h4>
+To browse source code visit <a id="normal" href="http://72.9.228.230:8080/svn/jsvn/">http://72.9.228.230:8080/svn/jsvn/</a>
+<br />To get the latest source code (with the SVN command line client) use the following command:
+<pre>$ svn co http://72.9.228.230:8080/svn/jsvn/trunk/</pre>
+<br>
+<p>
+Check the <a href="status.html">JavaSVN status</a> page to see
+what features are planned, but not yet implemented.
+</p>
+<br>
+<table width="100%">
+<tbody>
+<tr><td width="30%"><span style="font-size: 12pt"><b>Download</b></span></td>
+<td align="right" width="70%"><a style="text-decoration: none; border: 0px;" href="rss.xml"><span style="font-family:Arial;font-size:9px;background: #ff6000;color: #fff;padding: 0 4px">rss&nbsp;1.0</span>
+<a style="text-decoration: none; border: 0px;" href="rss2.xml"><span style="font-family:Arial;font-size:9px;background: #ff6000;color: #fff;padding: 0 4px">rss&nbsp;2.0</span></td>
+</tr>
+<tr><td style="padding-bottom: 5px;" colspan=2><b>Note:</b> to get an Eclipse plugin version use <b>http://tmate.org/svn/</b> as an update site location in Eclipse Update Manager.<br/></td></tr>
+
+<?
+include("feed/rss_util.php");
+publish_html("http://tmate.org/svn/");
+?>
+</tbody></table>
+<br>
+
+<table style="margin-top: 1em;" width="100%" cellpadding="0" cellspacing="0"><tr><td id="footer" align="left">Copyright &copy; 
+2004-2005, TMate Software</td><td align="right" id="footer">feedback is welcome at <a href="mailto:feedback%40tmatesoft.com">feedback@tmatesoft.com</a></td></tr></table>
+
+</td>
+</tr>
+</table>
+</div>
+</div>
+<!--</div>-->
+</td>
+</tr>
+</table>
+
+</body>
+</html>
