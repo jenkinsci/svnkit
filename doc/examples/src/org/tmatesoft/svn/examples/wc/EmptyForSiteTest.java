f1386fcc63754d97966fd5b525db930055bfeddf
sa <>
2005-07-30 18:26:57 +0000

    test continues - reverted previous test

---
+++ doc/examples/src/org/tmatesoft/svn/examples/wc/EmptyForSiteTest.java
@@ -0,0 +1,11 @@
+/*
+ * Created on 31.07.2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.tmatesoft.svn.examples.wc;
+
+public class EmptyForSiteTest {
+
+}
