b1b6a6275fc1c5aa803f5b7934091f298f385c2a
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2005-08-29 20:45:50 +0000

    SVNClient: extension to JavaHL - PromptUserPassword interface extension with methods to get private key and passphrase for the key.

---
+++ javasvn/src/org/tmatesoft/svn/core/javahl/PromptUserPassword4.java
@@ -0,0 +1,27 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://tmate.org/svn/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.core.javahl;
+
+import org.tigris.subversion.javahl.PromptUserPassword3;
+
+
+/**
+ * @version 1.0
+ * @author  TMate Software Ltd.
+ */
+public interface PromptUserPassword4 extends PromptUserPassword3 {
+    
+    public String getSSHPrivateKeyPath();
+    
+    public String getSSHPrivateKeyPassphrase();
+
+}
