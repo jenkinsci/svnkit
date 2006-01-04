a33715f0b212b3508ab976e0c42e1e0d3ce93620
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2006-01-04 18:44:22 +0000

    references to jsch library removed from web site pages and batch files

--- javasvn/cli/svn.bat
+++ javasvn/cli/svn.bat
@@ -4,7 +4,7 @@ set DEFAULT_JAVASVN_HOME=%~dp0
 
 if "%JAVASVN_HOME%"=="" set JAVASVN_HOME=%DEFAULT_JAVASVN_HOME%
 
-set JAVASVN_CLASSPATH= "%JAVASVN_HOME%javasvn.jar";"%JAVASVN_HOME%javasvn-cli.jar";"%JAVASVN_HOME%jsch.jar";"%JAVASVN_HOME%ganymed.jar"
+set JAVASVN_CLASSPATH= "%JAVASVN_HOME%javasvn.jar";"%JAVASVN_HOME%javasvn-cli.jar";"%JAVASVN_HOME%ganymed.jar"
 set JAVASVN_MAINCLASS=org.tmatesoft.svn.cli.SVN
 set JAVASVN_OPTIONS=-Djava.util.logging.config.file=%JAVASVN_HOME%/logging.properties
 
