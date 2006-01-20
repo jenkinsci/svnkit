2148a10e8198ea8308aa625f65218da7c3988737
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2006-01-20 13:46:11 +0000

    new line added to the long error message

--- javasvn/src/org/tmatesoft/svn/core/internal/wc/SVNDirectory.java
+++ javasvn/src/org/tmatesoft/svn/core/internal/wc/SVNDirectory.java
@@ -994,7 +994,7 @@ public class SVNDirectory {
             return entry;
         } else if (entry != null && entry.getKind() != fileKind) {
             SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.WC_NODE_KIND_CHANGE, "Can''t replace ''{0}'' with a node of different type;" +
-                    "commit the deletion, update the parent and then add ''{0}''", file);            
+                    "\ncommit the deletion, update the parent and then add ''{0}''", file);            
             SVNErrorManager.error(err);
         } else if (entry == null && SVNWCUtil.isVersionedDirectory(file)) {
             if (!force) {
