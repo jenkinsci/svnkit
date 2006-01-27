52647dc52e1681d698c1e3df78dcee85cdf886ac
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2006-01-27 14:41:28 +0000

    symlink to a versioned directory shouldn't be treated as a versioned dir

--- javasvn/src/org/tmatesoft/svn/core/internal/wc/SVNDirectory.java
+++ javasvn/src/org/tmatesoft/svn/core/internal/wc/SVNDirectory.java
@@ -980,13 +980,11 @@ public class SVNDirectory {
             SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.WC_PATH_NOT_FOUND, "''{0}'' not found", file);            
             SVNErrorManager.error(err);
         }
-        SVNNodeKind fileKind = fileType == SVNFileType.NONE
-                || fileType == SVNFileType.DIRECTORY ? SVNNodeKind.DIR
-                : SVNNodeKind.FILE;
+        SVNNodeKind fileKind = fileType == SVNFileType.NONE || fileType == SVNFileType.DIRECTORY ? 
+                SVNNodeKind.DIR : SVNNodeKind.FILE;
         SVNEntry entry = getEntries().getEntry(name, true);
 
-        if (entry != null && !entry.isDeleted()
-                && !entry.isScheduledForDeletion()) {
+        if (entry != null && !entry.isDeleted() && !entry.isScheduledForDeletion()) {
             if (!force) {
                 SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.ENTRY_EXISTS, "''{0}'' already under version control", file);            
                 SVNErrorManager.error(err);
@@ -996,7 +994,7 @@ public class SVNDirectory {
             SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.WC_NODE_KIND_CHANGE, "Can''t replace ''{0}'' with a node of different type;" +
                     "\ncommit the deletion, update the parent and then add ''{0}''", file);            
             SVNErrorManager.error(err);
-        } else if (entry == null && SVNWCUtil.isVersionedDirectory(file)) {
+        } else if (entry == null && fileType != SVNFileType.SYMLINK && SVNWCUtil.isVersionedDirectory(file)) {
             if (!force) {
                 SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.ENTRY_EXISTS, "''{0}'' already under version control", file);            
                 SVNErrorManager.error(err);
