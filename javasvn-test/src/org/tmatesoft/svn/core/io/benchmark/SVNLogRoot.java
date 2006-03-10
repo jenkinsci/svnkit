25ec34a5bc25f51e8a80e1122e897fa2d7b8340a
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2006-03-10 00:23:05 +0000

    simple benchmarking system to compare different protocol performance

---
+++ javasvn-test/src/org/tmatesoft/svn/core/io/benchmark/SVNLogRoot.java
@@ -0,0 +1,33 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004-2006 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://tmate.org/svn/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.core.io.benchmark;
+
+import org.tmatesoft.svn.core.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.io.SVNRepository;
+
+class SVNLogRoot extends SVNMeasurable implements Runnable {
+
+    protected void measure(SVNRepository repos) throws SVNException {
+        long latest = repos.getLatestRevision();
+        repos.log(null, 0, latest, true, false, 0, new ISVNLogEntryHandler() {
+            public void handleLogEntry(SVNLogEntry logEntry) throws SVNException {
+            }
+        });
+    }
+
+    protected String getName() {
+        return "log(/)";
+    }
+    
+}
