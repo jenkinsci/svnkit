8103892416240f0ec9726c5b3298fef19bfb927d
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2007-08-03 15:20:34 +0000

    cli2: XML support added for status command

--- svnkit-cli/src/org/tmatesoft/svn/cli2/command/SVNStatusCommand.java
+++ svnkit-cli/src/org/tmatesoft/svn/cli2/command/SVNStatusCommand.java
@@ -20,25 +20,27 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.tmatesoft.svn.cli2.SVNCommand;
 import org.tmatesoft.svn.cli2.SVNCommandUtil;
 import org.tmatesoft.svn.cli2.SVNNotifyPrinter;
 import org.tmatesoft.svn.cli2.SVNOption;
+import org.tmatesoft.svn.cli2.SVNXMLCommand;
 import org.tmatesoft.svn.core.SVNErrorCode;
 import org.tmatesoft.svn.core.SVNErrorMessage;
 import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.util.SVNDate;
 import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.wc.ISVNStatusHandler;
 import org.tmatesoft.svn.core.wc.SVNChangelistClient;
 import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.wc.SVNStatus;
 import org.tmatesoft.svn.core.wc.SVNStatusClient;
+import org.tmatesoft.svn.core.wc.SVNStatusType;
 
 /**
  * @version 1.1.2
  * @author  TMate Software Ltd.
  */
-public class SVNStatusCommand extends SVNCommand implements ISVNStatusHandler {
+public class SVNStatusCommand extends SVNXMLCommand implements ISVNStatusHandler {
 
     private SVNStatusPrinter myStatusPrinter;
     private Map myStatusCache;
@@ -86,14 +88,37 @@ public class SVNStatusCommand extends SVNCommand implements ISVNStatusHandler {
         if (!getEnvironment().isXML()) {
             client.setEventHandler(new SVNNotifyPrinter(getEnvironment()));
         }
+        if (getEnvironment().isXML()) {
+            if (!getEnvironment().isIncremental()) {
+                printXMLHeader("status");
+            }
+        } else if (getEnvironment().isIncremental()) {
+            SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.CL_ARG_PARSING_ERROR, "'incremental' option only valid in XML mode");
+            SVNErrorManager.error(err);
+        }
         for (Iterator ts = targets.iterator(); ts.hasNext();) {
             String target = (String) ts.next();
             getEnvironment().setOperatingPath(target, new File(target).getAbsoluteFile());
+
+            if (getEnvironment().isXML()) {
+                StringBuffer xmlBuffer = openXMLTag("target", XML_STYLE_NORMAL, "path", SVNCommandUtil.getLocalPath(target), null);
+                getEnvironment().getOut().print(xmlBuffer);
+            }
+            
             try {
-                client.doStatus(getEnvironment().getOperatingFile(), SVNRevision.HEAD,
+                long rev = client.doStatus(getEnvironment().getOperatingFile(), SVNRevision.HEAD,
                         getEnvironment().getDepth(), getEnvironment().isUpdate(),
                         getEnvironment().isVerbose(), getEnvironment().isNoIgnore(),
                         false, this);
+
+                if (getEnvironment().isXML()) {
+                    StringBuffer xmlBuffer = new StringBuffer();
+                    if (rev >= 0) {
+                        xmlBuffer = openXMLTag("against", XML_STYLE_SELF_CLOSING, "revision", Long.toString(rev), xmlBuffer);
+                    }
+                    xmlBuffer = closeXMLTag("target", xmlBuffer);
+                    getEnvironment().getOut().print(xmlBuffer);
+                }
             } catch (SVNException e) {
                 getEnvironment().handleWarning(e.getErrorMessage(), new SVNErrorCode[] {SVNErrorCode.WC_NOT_DIRECTORY});
             }
@@ -112,6 +137,9 @@ public class SVNStatusCommand extends SVNCommand implements ISVNStatusHandler {
                 }
             }
         }
+        if (getEnvironment().isXML() && !getEnvironment().isIncremental()) {
+            printXMLFooter("status");
+        }
     }
 
     public void handleStatus(SVNStatus status) throws SVNException {
@@ -127,8 +155,73 @@ public class SVNStatusCommand extends SVNCommand implements ISVNStatusHandler {
             ((Map) myStatusCache.get(status.getChangelistName())).put(path, status);
             return;
         }
-        myStatusPrinter.printStatus(path, status, 
+        if (getEnvironment().isXML()) {
+            if (status.getContentsStatus() == SVNStatusType.STATUS_NONE && status.getRemoteContentsStatus() == SVNStatusType.STATUS_NONE) {
+                return;
+            }
+            StringBuffer xmlBuffer = printXMLStatus(status, path);
+            getEnvironment().getOut().print(xmlBuffer);
+        } else {
+            myStatusPrinter.printStatus(path, status, 
                 getEnvironment().isVerbose() || getEnvironment().isUpdate(), 
                 getEnvironment().isVerbose(), getEnvironment().isQuiet(), getEnvironment().isUpdate());
+        }
+    }
+
+    protected StringBuffer printXMLStatus(SVNStatus status, String path) {
+        StringBuffer xmlBuffer = openXMLTag("entry", XML_STYLE_NORMAL, "path", path, null);
+        Map xmlMap = new LinkedHashMap();
+        xmlMap.put("props", status.getPropertiesStatus().toString());
+        xmlMap.put("item", status.getContentsStatus().toString());
+        if (status.isLocked()) {
+            xmlMap.put("wc-locked", "true");
+        }
+        if (status.isCopied()) {
+            xmlMap.put("copied", "true");
+        }
+        if (status.isSwitched()) {
+            xmlMap.put("switched", "true");
+        }
+        if (status.getEntry() != null && !status.isCopied()) {
+            xmlMap.put("revision", status.getRevision().toString());
+        }
+        xmlBuffer = openXMLTag("wc-status", XML_STYLE_NORMAL, xmlMap, xmlBuffer);
+        if (status.getEntry() != null && status.getCommittedRevision().isValid()) {
+            xmlBuffer = openXMLTag("commit", XML_STYLE_NORMAL, "revision", status.getCommittedRevision().toString(), xmlBuffer);
+            xmlBuffer = openCDataTag("author", status.getAuthor(), xmlBuffer);
+            if (status.getCommittedDate() != null) {
+                xmlBuffer = openCDataTag("date", ((SVNDate) status.getCommittedDate()).format(), xmlBuffer);
+            }
+            xmlBuffer = closeXMLTag("commit", xmlBuffer);
+        }
+        if (status.getEntry() != null && status.getLocalLock() != null) {
+            xmlBuffer = openXMLTag("lock", XML_STYLE_NORMAL, null, xmlBuffer);
+            xmlBuffer = openCDataTag("token", status.getLocalLock().getID(), xmlBuffer);
+            xmlBuffer = openCDataTag("owner", status.getLocalLock().getOwner(), xmlBuffer);
+            xmlBuffer = openCDataTag("comment", status.getLocalLock().getComment(), xmlBuffer);
+            xmlBuffer = openCDataTag("created", ((SVNDate) status.getLocalLock().getCreationDate()).format(), xmlBuffer);
+            xmlBuffer = closeXMLTag("lock", xmlBuffer);
+        }
+        xmlBuffer = closeXMLTag("wc-status", xmlBuffer);
+        if (status.getRemoteContentsStatus() != SVNStatusType.STATUS_NONE || status.getRemotePropertiesStatus() != SVNStatusType.STATUS_NONE ||
+                status.getRemoteLock() != null) {
+            xmlMap.put("props", status.getRemotePropertiesStatus().toString());
+            xmlMap.put("item", status.getRemoteContentsStatus().toString());
+            xmlBuffer = openXMLTag("repos-status", XML_STYLE_NORMAL, xmlMap, xmlBuffer);
+            if (status.getRemoteLock() != null) {
+                xmlBuffer = openXMLTag("lock", XML_STYLE_NORMAL, null, xmlBuffer);
+                xmlBuffer = openCDataTag("token", status.getRemoteLock().getID(), xmlBuffer);
+                xmlBuffer = openCDataTag("owner", status.getRemoteLock().getOwner(), xmlBuffer);
+                xmlBuffer = openCDataTag("comment", status.getRemoteLock().getComment(), xmlBuffer);
+                xmlBuffer = openCDataTag("created", ((SVNDate) status.getRemoteLock().getCreationDate()).format(), xmlBuffer);
+                if (status.getRemoteLock().getExpirationDate() != null) {
+                    xmlBuffer = openCDataTag("expires", ((SVNDate) status.getRemoteLock().getExpirationDate()).format(), xmlBuffer);
+                }
+                xmlBuffer = closeXMLTag("lock", xmlBuffer);
+            }
+            xmlBuffer = closeXMLTag("repos-status", xmlBuffer);
+        }
+        xmlBuffer = closeXMLTag("entry", xmlBuffer);
+        return xmlBuffer;
     }
 }
