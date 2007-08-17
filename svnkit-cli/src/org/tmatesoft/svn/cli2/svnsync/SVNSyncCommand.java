ebfecc0120d7efe7a20b5b0181807b762e030c24
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2007-08-17 18:58:39 +0000

    cli2: svnsync stub added.

---
+++ svnkit-cli/src/org/tmatesoft/svn/cli2/svnsync/SVNSyncCommand.java
@@ -0,0 +1,31 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004-2007 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://svnkit.com/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.cli2.svnsync;
+
+import org.tmatesoft.svn.cli2.AbstractSVNCommand;
+
+
+/**
+ * @version 1.1.2
+ * @author  TMate Software Ltd.
+ */
+public abstract class SVNSyncCommand extends AbstractSVNCommand {
+
+    public SVNSyncCommand(String name, String[] aliases) {
+        super(name, aliases);
+    }
+
+    protected String getResourceBundleName() {
+        return "org.tmatesoft.svn.cli2.svnsync.commands";
+    }
+
+}
---
+++ svnkit-cli/src/org/tmatesoft/svn/cli2/svnsync/SVNSyncCommandEnvironment.java
@@ -0,0 +1,49 @@
+/*
+ * ====================================================================
+ * Copyright (c) 2004-2007 TMate Software Ltd.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://svnkit.com/license.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+package org.tmatesoft.svn.cli2.svnsync;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.tmatesoft.svn.cli2.AbstractSVNCommandEnvironment;
+import org.tmatesoft.svn.cli2.SVNOptionValue;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.wc.ISVNOptions;
+
+
+/**
+ * @version 1.1.2
+ * @author  TMate Software Ltd.
+ */
+public class SVNSyncCommandEnvironment extends AbstractSVNCommandEnvironment {
+
+    public SVNSyncCommandEnvironment(String programName, PrintStream out, PrintStream err, InputStream in) {
+        super(programName, out, err, in);
+    }
+
+    protected ISVNAuthenticationManager createClientAuthenticationManager() {
+        return null;
+    }
+
+    protected ISVNOptions createClientOptions() {
+        return null;
+    }
+
+    protected void initOption(SVNOptionValue optionValue) throws SVNException {
+    }
+
+    protected String refineCommandName(String commandName) throws SVNException {
+        return null;
+    }
+
+}
