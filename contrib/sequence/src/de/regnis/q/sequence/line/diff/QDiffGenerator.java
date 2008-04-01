a93ed0cfd72a4c5f92513118ff72e61e3b0f478b
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2008-04-01 17:30:14 +0000

    Updated/added missing Sequence library file license headers and corrected contact information in SEQUENCE-LICENSE.

--- contrib/sequence/src/de/regnis/q/sequence/line/diff/QDiffGenerator.java
+++ contrib/sequence/src/de/regnis/q/sequence/line/diff/QDiffGenerator.java
@@ -1,12 +1,11 @@
 /*
  * ====================================================================
- * Copyright (c) 2004 TMate Software Ltd.  All rights reserved.
+ * Copyright (c) 2000-2008 SyntEvo GmbH, info@syntevo.com
+ * All rights reserved.
  *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at http://svnkit.com/license.html
- * If newer versions of this license are posted there, you may use a
- * newer version instead, at your option.
+ * This software is licensed as described in the file SEQUENCE-LICENSE,
+ * which you should have received as part of this distribution. Use is
+ * subject to license terms.
  * ====================================================================
  */
 
