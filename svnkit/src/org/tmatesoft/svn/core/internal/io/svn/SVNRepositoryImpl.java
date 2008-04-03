ba1311fdffb7fcde7519e5fcfdf5a6dcec86d469
Semen Vadishev <semen.vadishev@tmatesoft.com>
2008-04-03 13:23:37 +0000

    SVNRepositoryImpl.getMergeInfoImpl() fixed: method should return map were keys are absolute paths (relative to repository root)

--- svnkit/src/org/tmatesoft/svn/core/internal/io/svn/SVNRepositoryImpl.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/io/svn/SVNRepositoryImpl.java
@@ -1489,7 +1489,8 @@ public class SVNRepositoryImpl extends SVNRepository implements ISVNReporter {
                 }
                 List values = SVNReader.parseTuple("ss", item.getItems(), null);
                 String path = SVNReader.getString(values, 0);
-                path = path.startsWith("/") ? path : "/" + path;
+                path = path.charAt(0) == '/' ? path.substring(1) : path;
+                path = getRepositoryPath(path);
                 String mergeInfoToParse = SVNReader.getString(values, 1);
                 Map srcsToRangeLists = SVNMergeInfoUtil.parseMergeInfo(new StringBuffer(mergeInfoToParse), null);
                 SVNMergeInfo mergeInfo = new SVNMergeInfo(path, srcsToRangeLists);
