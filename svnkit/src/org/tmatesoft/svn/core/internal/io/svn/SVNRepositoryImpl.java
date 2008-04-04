37447001702f7c5f1abfb7de99becb4e792633c6
Semen Vadishev <semen.vadishev@tmatesoft.com>
2008-04-04 16:18:12 +0000

    SVNRepositoryImpl.getMergeInfoImpl() fixed: OOBE removed

--- svnkit/src/org/tmatesoft/svn/core/internal/io/svn/SVNRepositoryImpl.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/io/svn/SVNRepositoryImpl.java
@@ -1489,7 +1489,7 @@ public class SVNRepositoryImpl extends SVNRepository implements ISVNReporter {
                 }
                 List values = SVNReader.parseTuple("ss", item.getItems(), null);
                 String path = SVNReader.getString(values, 0);
-                path = path.charAt(0) == '/' ? path.substring(1) : path;
+                path = path.startsWith("/") ? path.substring(1) : path;
                 path = getRepositoryPath(path);
                 String mergeInfoToParse = SVNReader.getString(values, 1);
                 Map srcsToRangeLists = SVNMergeInfoUtil.parseMergeInfo(new StringBuffer(mergeInfoToParse), null);
