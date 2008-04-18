72962c0c798d9c11dd3803ff2e33473f3df81937
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2008-04-18 17:08:45 +0000

    cli2: jsvnversion didn't work, fixed.

--- svnkit-cli/src/org/tmatesoft/svn/cli2/svnversion/SVNVersionCommandEnvironment.java
+++ svnkit-cli/src/org/tmatesoft/svn/cli2/svnversion/SVNVersionCommandEnvironment.java
@@ -24,11 +24,8 @@ import org.tmatesoft.svn.cli2.AbstractSVNCommandEnvironment;
 import org.tmatesoft.svn.cli2.AbstractSVNOption;
 import org.tmatesoft.svn.cli2.SVNCommandLine;
 import org.tmatesoft.svn.cli2.SVNOptionValue;
-import org.tmatesoft.svn.core.SVNErrorCode;
-import org.tmatesoft.svn.core.SVNErrorMessage;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
-import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.wc.ISVNOptions;
 import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
@@ -101,31 +98,27 @@ public class SVNVersionCommandEnvironment extends AbstractSVNCommandEnvironment 
             return "help";
         } 
 
-        if (commandName == null) {
-            if (isVersion()) {
-                SVNVersionCommand versionCommand = new SVNVersionCommand() {
-                    protected Collection createSupportedOptions() {
-                        LinkedList options = new LinkedList();
-                        options.add(SVNVersionOption.VERSION);
-                        return options;
-                    }
-                    
-                    public void run() throws SVNException {
-                        AbstractSVNCommand helpCommand = AbstractSVNCommand.getCommand("help");
-                        helpCommand.init(SVNVersionCommandEnvironment.this);
-                        helpCommand.run();
-                    }
+        if (isVersion()) {
+            SVNVersionCommand versionCommand = new SVNVersionCommand() {
+                protected Collection createSupportedOptions() {
+                    LinkedList options = new LinkedList();
+                    options.add(SVNVersionOption.VERSION);
+                    return options;
+                }
+                
+                public void run() throws SVNException {
+                    AbstractSVNCommand helpCommand = AbstractSVNCommand.getCommand("help");
+                    helpCommand.init(SVNVersionCommandEnvironment.this);
+                    helpCommand.run();
+                }
 
-                    public String getName() {
-                        return "--version";
-                    }
-                };
-                AbstractSVNCommand.registerCommand(versionCommand);
-                return "--version";
-            }
-            SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.CL_INSUFFICIENT_ARGS, "Subcommand argument required");
-            SVNErrorManager.error(err);
+                public String getName() {
+                    return "--version";
+                }
+            };
+            AbstractSVNCommand.registerCommand(versionCommand);
+            return "--version";
         }
-        return commandName;
+        return "";
     }
 }
