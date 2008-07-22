fd788b91f99034764597620b14568d0b58c2205a
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2008-07-22 19:09:10 +0000

    merge: WC properties (those svn:wc:propname) shouldn't be merged in and be included into property changes.

--- svnkit/src/org/tmatesoft/svn/core/internal/wc/SVNMergeCallback.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/wc/SVNMergeCallback.java
@@ -290,7 +290,7 @@ public class SVNMergeCallback extends AbstractDiffCallback {
         SVNProperties newProps = new SVNProperties(originalProperties);
         for (Iterator propChangesIter = diff.nameSet().iterator(); propChangesIter.hasNext();) {
             String propName = (String) propChangesIter.next();
-            if (!myMergeDriver.myIsSameRepository && SVNProperty.isWorkingCopyProperty(propName)) {
+            if (SVNProperty.isWorkingCopyProperty(propName)) {
                 continue;
             }
             SVNPropertyValue propValue = diff.getSVNPropertyValue(propName);
