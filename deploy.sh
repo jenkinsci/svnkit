454152348d4d21241fa993a1e141cf5835e265a7
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-07-24 16:13:09 -0700

    set the version to indicate that this is beta-2 derivative

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 tiger
 ant clean build-src build-library
 
-version=1.2.0-hudson-1
+version=1.2.0-beta-2-hudson-1
 
 # be sure to update pom.xml and svnkit.build.properties
 
-#cmd=install:install-file
-cmd=deploy:deploy-file
+cmd=install:install-file
+#cmd=deploy:deploy-file
 mvn $cmd -Dfile=build/lib/svnkit.jar -DrepositoryId=java.net2 -Durl=file:$(cygpath -wa ~m2repo) -DpomFile=pom.xml
 mvn $cmd -Dfile=build/lib/svnkitsrc.zip -DrepositoryId=java.net2 -Durl=file:$(cygpath -wa ~m2repo) -DpomFile=pom.xml -Dclassifier=sources
