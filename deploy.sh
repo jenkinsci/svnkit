fb33bb76f32749f02394ea6bbdb0d1d3e7dadd5f
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-07-24 16:38:36 -0700

    going to deploy 1.2.0-beta-2-hudson-1

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ version=1.2.0-beta-2-hudson-1
 
 # be sure to update pom.xml and svnkit.build.properties
 
-cmd=install:install-file
-#cmd=deploy:deploy-file
+#cmd=install:install-file
+cmd=deploy:deploy-file
 mvn $cmd -Dfile=build/lib/svnkit.jar -DrepositoryId=java.net2 -Durl=file:$(cygpath -wa ~m2repo) -DpomFile=pom.xml
 mvn $cmd -Dfile=build/lib/svnkitsrc.zip -DrepositoryId=java.net2 -Durl=file:$(cygpath -wa ~m2repo) -DpomFile=pom.xml -Dclassifier=sources
