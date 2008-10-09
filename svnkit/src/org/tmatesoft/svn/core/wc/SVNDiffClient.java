3108bbee3f2bbd25dafdfbddf4579f176615b098
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2008-10-09 16:37:12 +0000

    merged from trunk: r4829: small code cleanup - obsolete 'continue' removed

--- svnkit/src/org/tmatesoft/svn/core/wc/SVNDiffClient.java
+++ svnkit/src/org/tmatesoft/svn/core/wc/SVNDiffClient.java
@@ -363,7 +363,6 @@ public class SVNDiffClient extends SVNMergeDriver {
                 doDiff(path, pegRevision, rN, rM, depth, useAncestry, result, changeLists);
             } catch (SVNException svne) {
                 dispatchEvent(SVNEventFactory.createErrorEvent(svne.getErrorMessage()));
-                continue;
             }
         }
     }
