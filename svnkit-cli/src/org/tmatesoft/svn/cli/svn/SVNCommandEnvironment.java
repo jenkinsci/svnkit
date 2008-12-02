a1065e77fbc8073bc7b274b6e9d2c8d5fe523e8c
sa <>
2008-12-02 11:44:01 +0000

    ported Subversion bugfix (r34487): compatibility bug: it was not possible to commit only property changes on a folder with modified child items using jsvn command line client

--- svnkit-cli/src/org/tmatesoft/svn/cli/svn/SVNCommandEnvironment.java
+++ svnkit-cli/src/org/tmatesoft/svn/cli/svn/SVNCommandEnvironment.java
@@ -547,8 +547,12 @@ public class SVNCommandEnvironment extends AbstractSVNCommandEnvironment impleme
         if (!myIsDescend) {
             if (getCommand() instanceof SVNStatusCommand) {
                 myDepth = SVNDepth.IMMEDIATES;
+            } else if (getCommand() instanceof SVNRevertCommand ||
+                    getCommand() instanceof SVNAddCommand ||
+                    getCommand() instanceof SVNCommitCommand) {
+                myDepth = SVNDepth.EMPTY;
             } else {
-                myDepth = SVNDepth.fromRecurse(myIsDescend);
+                myDepth = SVNDepth.FILES;
             }
         }
     }
