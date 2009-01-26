9bc87e54442ed53ead00d0463f617435f07e7b44
Semen Vadishev <semen.vadishev@tmatesoft.com>
2009-01-26 13:57:18 +0000

    extended merge tests updated

--- svnkit-test/src/org/tmatesoft/svn/test/tests/merge/ext/ChangeMergeTargetCopyFromPathTest3.java
+++ svnkit-test/src/org/tmatesoft/svn/test/tests/merge/ext/ChangeMergeTargetCopyFromPathTest3.java
@@ -15,13 +15,12 @@ import java.io.File;
 
 import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.wc.SVNCopySource;
 import org.tmatesoft.svn.core.wc.SVNCopyTask;
 import org.tmatesoft.svn.core.wc.SVNEditorAction;
 import org.tmatesoft.svn.core.wc.SVNRevision;
-import org.tmatesoft.svn.test.util.SVNTestDebugLog;
 import org.tmatesoft.svn.test.wc.SVNTestFileDescriptor;
 import org.tmatesoft.svn.test.wc.SVNWCDescriptor;
 
@@ -29,7 +28,7 @@ import org.tmatesoft.svn.test.wc.SVNWCDescriptor;
  * @author TMate Software Ltd.
  * @version 1.2.0
  */
-public class ChangeMergeTargetCopyFromPathTest extends AbstractExtMergeTest {
+public class ChangeMergeTargetCopyFromPathTest3 extends AbstractExtMergeTest {
 
     public ISVNTestExtendedMergeCallback getFeatureModeCallback() {
         return new FeatureModeCallback();
@@ -54,16 +53,21 @@ public class ChangeMergeTargetCopyFromPathTest extends AbstractExtMergeTest {
         fill();
 
         createWCs();
+
         initializeMergeCallback();
 
-        getEnvironment().copy(getBranchFile("A/file"), SVNRevision.WORKING, getBranchFile("A/file2"), true, false, true);
-        getEnvironment().commit(getBranchFile("A"), "A/file renamed to A/file2", SVNDepth.INFINITY);
+        getEnvironment().write(getBranchFile("B/file"), "this is B/file", false);
+        getEnvironment().add(getBranchFile("B"), false, SVNDepth.INFINITY, false);
+        getEnvironment().commit(getBranchWC(), "Added B", SVNDepth.INFINITY);
 
-        getEnvironment().modifyAndCommit(getBranchFile("A/file2"));
+        mergeLastRevisions(getBranch(), getTrunkWC(), 1, SVNDepth.INFINITY, false, false);
+        getEnvironment().commit(getTrunkWC(), "Merged B from branch", SVNDepth.INFINITY);
 
-        getEnvironment().setEventHandler(SVNTestDebugLog.getEventHandler());
+        getEnvironment().copy(getBranchFile("B/file"), SVNRevision.WORKING, getBranchFile("B/file2"), true, false, true);
+        getEnvironment().commit(getBranchFile("B"), "Renamed B/file to B/file2", SVNDepth.INFINITY);
+	    final long lastRevision = getEnvironment().modifyAndCommit(getBranchFile("B/file2"));
 
-        mergeLastRevisions(getBranch(), getTrunkWC(), 2, SVNDepth.INFINITY, false, false);
+	    mergeLastRevisions(getBranch(), getTrunkWC(), lastRevision, SVNDepth.INFINITY, false, false);
         validateWC(getTrunkWC());
     }
 
@@ -75,26 +79,26 @@ public class ChangeMergeTargetCopyFromPathTest extends AbstractExtMergeTest {
         }
 
         public SVNCopyTask getTargetCopySource(SVNURL sourceUrl, long sourceRevision, long sourceMergeFromRevision, long sourceMergeToRevision, SVNURL targetUrl, long targetRevision) throws SVNException {
-            if (targetUrl.getPath().endsWith("trunk/A/file2")) {
-                SVNCopySource copySource = new SVNCopySource(SVNRevision.create(targetRevision), SVNRevision.create(targetRevision), getTrunk().appendPath("A/file", false));
+            if (targetUrl.getPath().endsWith("trunk/B/file2")) {
+                SVNCopySource copySource = new SVNCopySource(SVNRevision.create(targetRevision), SVNRevision.create(targetRevision), getTrunk().appendPath("B/file", false));
                 return SVNCopyTask.create(copySource, true);
             }
             return null;
         }
 
         public SVNURL[] getTrueMergeTargets(SVNURL sourceUrl, long sourceRevision, long sourceMergeFromRevision, long sourceMergeToRevision, SVNURL targetUrl, long targetRevision, SVNEditorAction action) throws SVNException {
-            if (action == SVNEditorAction.DELETE && sourceUrl.getPath().endsWith("branch/A/file")) {
+            if (action == SVNEditorAction.DELETE && sourceUrl.getPath().endsWith("branch/B/file")) {
                 return new SVNURL[0];
             }
-            if (sourceUrl.getPath().endsWith("branch/A/file2")) {
-                return new SVNURL[]{getTrunk().appendPath("A/file2", false)};
+            if (sourceUrl.getPath().endsWith("branch/B/file2")) {
+                return new SVNURL[]{getTrunk().appendPath("B/file2", false)};
             }
             return null;
         }
 
         public SVNURL transformLocation(SVNURL sourceUrl, long sourceRevision, long targetRevision) throws SVNException {
-            if (sourceUrl.getPath().endsWith("branch/A/file2")) {
-                return getBranch().appendPath("A/file", false);
+            if (sourceUrl.getPath().endsWith("branch/B/file2")) {
+                return getBranch().appendPath("B/file", false);
             }
             return null;
         }
@@ -130,18 +134,18 @@ public class ChangeMergeTargetCopyFromPathTest extends AbstractExtMergeTest {
         }
 
         public SVNURL[] getTrueMergeTargets(SVNURL sourceUrl, long sourceRevision, long sourceMergeFromRevision, long sourceMergeToRevision, SVNURL targetUrl, long targetRevision, SVNEditorAction action) throws SVNException {
-            if (sourceUrl.getPath().endsWith("branch/A/file2")) {
-                return new SVNURL[]{getTrunk().appendPath("A/file", false)};
+            if (sourceUrl.getPath().endsWith("branch/B/file2")) {
+                return new SVNURL[]{getTrunk().appendPath("B/file", false)};
             }
-            if (sourceUrl.getPath().endsWith("branch/A/file")) {
+            if (sourceUrl.getPath().endsWith("branch/B/file") && sourceRevision > 3) {
                 return new SVNURL[0];
             }
             return null;
         }
 
         public SVNURL transformLocation(SVNURL sourceUrl, long sourceRevision, long targetRevision) throws SVNException {
-            if (sourceUrl.getPath().endsWith("branch/A/file2")) {
-                return getBranch().appendPath("A/file", false);
+            if (sourceUrl.getPath().endsWith("branch/B/file2")) {
+                return getBranch().appendPath("B/file", false);
             }
             return null;
         }
