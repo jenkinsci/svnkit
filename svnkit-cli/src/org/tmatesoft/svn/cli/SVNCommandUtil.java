97269a27be72a93b2c0cb3a72fa1ec40777f4c07
sa <>
2009-04-15 15:57:14 +0000

    /branches/1.3.x:
    SVNCommandUtil.java:
     - changed years in the version string from 2004-2008 
       to 2004-2009

--- svnkit-cli/src/org/tmatesoft/svn/cli/SVNCommandUtil.java
+++ svnkit-cli/src/org/tmatesoft/svn/cli/SVNCommandUtil.java
@@ -434,7 +434,7 @@ public class SVNCommandUtil {
         }
         if (!quiet) {
             message += 
-                "\nCopyright (c) 2004-2008 TMate Software.\n" +
+                "\nCopyright (c) 2004-2009 TMate Software.\n" +
                 "SVNKit is open source (GPL) software, see http://svnkit.com/ for more information.\n" +
                 "SVNKit is pure Java (TM) version of Subversion, see http://subversion.tigris.org/";
         }
