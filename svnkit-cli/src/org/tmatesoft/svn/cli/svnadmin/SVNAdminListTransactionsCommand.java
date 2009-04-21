746aa8281cb692020b4a1456a67762487af71f48
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2009-04-21 13:49:59 +0000

    copyright year updated in source code files

--- svnkit-cli/src/org/tmatesoft/svn/cli/svnadmin/SVNAdminListTransactionsCommand.java
+++ svnkit-cli/src/org/tmatesoft/svn/cli/svnadmin/SVNAdminListTransactionsCommand.java
@@ -1,6 +1,6 @@
 /*
  * ====================================================================
- * Copyright (c) 2004-2008 TMate Software Ltd.  All rights reserved.
+ * Copyright (c) 2004-2009 TMate Software Ltd.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
