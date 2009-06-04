fdc6ffb97593427d88d5ba1523a9fda8163b17ec
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2009-06-04 16:21:35 +0000

    merged from trunk: r5863: incorrect copyfrom path was written to the entries file on merging a single file.

--- svnkit/src/org/tmatesoft/svn/core/internal/wc/SVNMergeCallback15.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/wc/SVNMergeCallback15.java
@@ -332,7 +332,11 @@ public class SVNMergeCallback15 extends SVNMergeCallback {
                 String copyFromURL = null;
                 long copyFromRevision = SVNRepository.INVALID_REVISION;
                 if (myMergeDriver.myIsSameRepository) {
-                    copyFromURL = myURL.appendPath(path, false).toString();
+                    String targePath = myMergeDriver.myTarget.getAbsolutePath();
+                    String minePath = mergedFile.getAbsolutePath();
+                    String relativePath = SVNPathUtil.getRelativePath(targePath, minePath);
+
+                    copyFromURL = myURL.appendPath(relativePath, false).toString();    
                     copyFromRevision = revision2;
                 }
                 // TODO compare protocols with dir one.
