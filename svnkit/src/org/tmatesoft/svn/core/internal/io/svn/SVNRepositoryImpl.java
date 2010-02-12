73e32688b842bae852a1bf3080605512a38b4612
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2010-02-12 14:25:02 +0000

    svn protocol: getLock should return null, not empty lock for not locked files.

--- svnkit/src/org/tmatesoft/svn/core/internal/io/svn/SVNRepositoryImpl.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/io/svn/SVNRepositoryImpl.java
@@ -938,7 +938,9 @@ public class SVNRepositoryImpl extends SVNRepository implements ISVNReporter {
                     SVNErrorManager.error(err, SVNLogType.NETWORK);
                 }
                 SVNLock lock = SVNReader.getLock(item.getItems());
-                locks.add(lock);
+                if (lock != null) {
+                    locks.add(lock);
+                }
             }
             return (SVNLock[]) locks.toArray(new SVNLock[locks.size()]);
         } catch (SVNException e) {
@@ -995,6 +997,9 @@ public class SVNRepositoryImpl extends SVNRepository implements ISVNReporter {
                     List items = (List) values.get(1);
                     if ("success".equals(status)) {
                         lock = SVNReader.getLock(items);
+                        if (lock == null) {
+                            continue;
+                        }
                         path = lock.getPath();
                     } else if ("failure".equals(status)) {
                         SVNReader.handleFailureStatus(items);
