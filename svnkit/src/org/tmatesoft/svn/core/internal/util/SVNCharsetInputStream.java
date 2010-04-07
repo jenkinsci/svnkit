ddafaca23ec9df8948f826765648f3f9f600d883
Semen Vadishev <semen.vadishev@tmatesoft.com>
2010-04-07 16:22:47 +0000

    Charset conversion: #toString() methods added.

--- svnkit/src/org/tmatesoft/svn/core/internal/util/SVNCharsetInputStream.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/util/SVNCharsetInputStream.java
@@ -11,14 +11,14 @@
  */
 package org.tmatesoft.svn.core.internal.util;
 
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.wc.IOExceptionWrapper;
+
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
-
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.internal.wc.IOExceptionWrapper;
 
 
 /**
@@ -92,5 +92,13 @@ public class SVNCharsetInputStream extends FilterInputStream {
             readed += r;
         }
         return readed;
+    }
+
+    public String toString() {
+        final StringBuilder buffer = new StringBuilder();
+        buffer.append("SVNCharsetInputStream");
+        buffer.append("[").append(myCharsetConvertor);
+        buffer.append(']');
+        return buffer.toString();
     }
 }
