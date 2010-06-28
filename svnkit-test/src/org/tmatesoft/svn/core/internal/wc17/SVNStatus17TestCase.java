de7d9885edc6faf9d8221adb576ef187e5eb23d9
sergey.scherbina <>
2010-06-28 01:30:28 +0000

    Development of SVN1.7 WC-NG support in progress (status)

--- svnkit-test/src/org/tmatesoft/svn/core/internal/wc17/SVNStatus17TestCase.java
+++ svnkit-test/src/org/tmatesoft/svn/core/internal/wc17/SVNStatus17TestCase.java
@@ -12,29 +12,38 @@
 package org.tmatesoft.svn.core.internal.wc17;
 
 import java.io.File;
+import java.util.Collection;
 
+import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
 import org.tmatesoft.svn.core.internal.wc.DefaultSVNOptions;
+import org.tmatesoft.svn.core.wc.ISVNStatusHandler;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNStatus;
+import org.tmatesoft.svn.examples.wc.StatusHandler;
 
 import junit.framework.TestCase;
 
-
 /**
  * @version 1.3
- * @author  TMate Software Ltd.
+ * @author TMate Software Ltd.
  */
 public class SVNStatus17TestCase extends TestCase {
-        
+
     public SVNStatus17TestCase() {
         super("SVNStatus17");
     }
 
-    public void testLocalStatus17() throws SVNException{
-        final SVNStatusClient17 client = new SVNStatusClient17(
-                new BasicAuthenticationManager("test","test"), 
-                new DefaultSVNOptions(null, true));
-        client.doStatus(new File(""), false);
+    public void testLocalStatus17() throws SVNException {
+        final SVNStatusClient17 client = new SVNStatusClient17(new BasicAuthenticationManager("test", "test"), new DefaultSVNOptions(null, true));
+        final SVNStatus status = client.doStatus(new File(""), false);
+        assert (status != null);
     }
-    
+
+    public void testLocalStatus17Recursive() throws SVNException {
+        final SVNStatusClient17 client = new SVNStatusClient17(new BasicAuthenticationManager("test", "test"), new DefaultSVNOptions(null, true));
+        long revision = client.doStatus(new File(""), SVNRevision.WORKING, SVNDepth.INFINITY, false, false, false, false, new StatusHandler(false), null);
+    }
+
 }
