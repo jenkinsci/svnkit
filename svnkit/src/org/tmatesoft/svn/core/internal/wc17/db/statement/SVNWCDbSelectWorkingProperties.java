6a93b91033e3c17bde68d3cfa15fb8158cbb8fb0
sergey.scherbina <>
2010-10-18 21:53:37 +0000

    SVN1.7 (wc-ng): switch to 'NODES' format in progress

--- svnkit/src/org/tmatesoft/svn/core/internal/wc17/db/statement/SVNWCDbSelectWorkingProperties.java
+++ svnkit/src/org/tmatesoft/svn/core/internal/wc17/db/statement/SVNWCDbSelectWorkingProperties.java
@@ -16,20 +16,24 @@ import org.tmatesoft.svn.core.internal.db.SVNSqlJetDb;
 import org.tmatesoft.svn.core.internal.db.SVNSqlJetSelectFieldsStatement;
 
 /**
- * SELECT properties, presence FROM WORKING_NODE WHERE wc_id = ?1 AND
- * local_relpath = ?2;
+ * SELECT properties, presence FROM nodes WHERE wc_id = ?1 AND local_relpath =
+ * ?2 AND op_depth > 0 ORDER BY op_depth DESC LIMIT 1;
  *
  * @author TMate Software Ltd.
  */
-public class SVNWCDbSelectWorkingProperties extends SVNSqlJetSelectFieldsStatement<SVNWCDbSchema.WORKING_NODE__Fields> {
+public class SVNWCDbSelectWorkingProperties extends SVNSqlJetSelectFieldsStatement<SVNWCDbSchema.NODES__Fields> {
 
     public SVNWCDbSelectWorkingProperties(SVNSqlJetDb sDb) throws SVNException {
-        super(sDb, SVNWCDbSchema.WORKING_NODE);
+        super(sDb, SVNWCDbSchema.NODES);
     }
 
     protected void defineFields() {
-        fields.add(SVNWCDbSchema.WORKING_NODE__Fields.properties);
-        fields.add(SVNWCDbSchema.WORKING_NODE__Fields.presence);
+        fields.add(SVNWCDbSchema.NODES__Fields.properties);
+        fields.add(SVNWCDbSchema.NODES__Fields.presence);
+    }
+
+    protected boolean isFilterPassed() throws SVNException {
+        return getColumnLong(SVNWCDbSchema.NODES__Fields.op_depth) > 0;
     }
 
 }
