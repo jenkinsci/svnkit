4a414371bd38025000c7265ad90625565b1ce3cf
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2010-11-15 14:46:46 +0000

    merged from trunk: reference to GPL (incorrect) removed from the command line client output.

--- svnkit-cli/src/org/tmatesoft/svn/cli/SVNCommandUtil.java
+++ svnkit-cli/src/org/tmatesoft/svn/cli/SVNCommandUtil.java
@@ -470,8 +470,8 @@ public class SVNCommandUtil {
         if (!quiet) {
             message += 
                 "\nCopyright (c) 2004-2010 TMate Software.\n" +
-                "SVNKit is open source (GPL) software, see http://svnkit.com/ for more information.\n" +
-                "SVNKit is pure Java (TM) version of Subversion, see http://subversion.tigris.org/";
+                "SVNKit is an Open Source software, see http://svnkit.com/ for more information.\n" +
+                "SVNKit is a pure Java (TM) version of Subversion, see http://subversion.tigris.org/";
         }
         return message;
     
