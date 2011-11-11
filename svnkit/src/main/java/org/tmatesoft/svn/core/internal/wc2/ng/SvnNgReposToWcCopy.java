cdd2b5838f24c07050a0c5bf97a7441d15562664
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2011-11-11 22:14:13 +0000

    WIP: copy operation.

--- svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgReposToWcCopy.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgReposToWcCopy.java
@@ -132,7 +132,7 @@ public class SvnNgReposToWcCopy extends SvnNgOperationRunner<Long, SvnCopy> {
                 
                 Structure<NodeOriginInfo> no = getWcContext().getNodeOrigin(src, true, NodeOriginInfo.reposRelpath, NodeOriginInfo.reposRootUrl, NodeOriginInfo.revision);
                 if (no.get(NodeOriginInfo.reposRelpath) != null) {
-                    pair.source = no.<SVNURL>get(NodeOriginInfo.reposRootUrl).appendPath(no.text(NodeOriginInfo.reposRelpath), false);
+                    pair.source = no.<SVNURL>get(NodeOriginInfo.reposRootUrl).appendPath(no.<File>get(NodeOriginInfo.reposRelpath).getPath(), false);
                 } else {
                     SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.ENTRY_MISSING_URL, "''{0}'' does not have an URL associated with it", src);
                     SVNErrorManager.error(err, SVNLogType.WC);
