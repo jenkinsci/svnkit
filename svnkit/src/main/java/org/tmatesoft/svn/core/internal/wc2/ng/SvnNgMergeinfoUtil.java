d0bf5c6f16508696ec4756e52abe8f7bafc8e598
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2012-01-06 20:59:31 +0000

    WIP: mergeinfo related operations for the new working copy format.

--- svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgMergeinfoUtil.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgMergeinfoUtil.java
@@ -8,18 +8,27 @@ import java.util.TreeMap;
 import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNErrorCode;
 import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNMergeInfo;
 import org.tmatesoft.svn.core.SVNMergeInfoInheritance;
 import org.tmatesoft.svn.core.SVNMergeRangeList;
 import org.tmatesoft.svn.core.SVNNodeKind;
 import org.tmatesoft.svn.core.SVNProperties;
 import org.tmatesoft.svn.core.SVNProperty;
 import org.tmatesoft.svn.core.SVNPropertyValue;
+import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.internal.util.SVNMergeInfoUtil;
 import org.tmatesoft.svn.core.internal.util.SVNPathUtil;
+import org.tmatesoft.svn.core.internal.util.SVNURLUtil;
 import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
 import org.tmatesoft.svn.core.internal.wc17.SVNWCContext;
 import org.tmatesoft.svn.core.internal.wc17.SVNWCContext.SVNWCNodeReposInfo;
+import org.tmatesoft.svn.core.internal.wc17.SVNWCUtils;
 import org.tmatesoft.svn.core.internal.wc17.db.SVNWCDb;
+import org.tmatesoft.svn.core.internal.wc17.db.Structure;
+import org.tmatesoft.svn.core.internal.wc17.db.StructureFields.NodeOriginInfo;
+import org.tmatesoft.svn.core.internal.wc2.SvnRepositoryAccess;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.wc2.ISvnObjectReceiver;
 import org.tmatesoft.svn.core.wc2.SvnTarget;
 
@@ -148,4 +157,139 @@ public class SvnNgMergeinfoUtil {
         }
         return result;
     }
-}
+    
+    private static SvnMergeInfoCatalogInfo getReposMergeInfoCatalog(SVNRepository repository, String relativePath, long revision, SVNMergeInfoInheritance inheritance, 
+            boolean squelchIncapable, boolean includeDescendats) throws SVNException {
+        
+        SvnMergeInfoCatalogInfo result = new SvnMergeInfoCatalogInfo();
+        Map<String, SVNMergeInfo> reposMeregInfo = null;
+        
+        try {
+            reposMeregInfo = repository.getMergeInfo(new String[] {relativePath}, revision, inheritance, includeDescendats);
+        } catch (SVNException e) {
+            if (e.getErrorMessage().getErrorCode() == SVNErrorCode.UNSUPPORTED_FEATURE && squelchIncapable) {
+                return result;
+            }
+            throw e;
+        }
+        
+        if (reposMeregInfo != null && !reposMeregInfo.isEmpty()) {
+            result.catalog = new TreeMap<String, Map<String,SVNMergeRangeList>>();
+            SVNURL location = repository.getLocation();
+            SVNURL root = repository.getRepositoryRoot(true);
+            String prefix = SVNURLUtil.getRelativeURL(root, location);
+            for (String reposRelativePath : reposMeregInfo.keySet()) {
+                SVNMergeInfo mi = reposMeregInfo.get(reposRelativePath);
+                if ("".equals(reposRelativePath)) {
+                    reposRelativePath = prefix;
+                } else {
+                    reposRelativePath = SVNPathUtil.append(prefix, reposRelativePath);
+                }
+                result.catalog.put(reposRelativePath, mi.getMergeSourcesToMergeLists());
+            }
+        }
+        return result;
+    }
+
+    private static SvnMergeInfoInfo getReposMergeInfo(SVNRepository repository, String relativePath, long revision, SVNMergeInfoInheritance inheritance, 
+            boolean squelchIncapable) throws SVNException {
+        SvnMergeInfoInfo result = new SvnMergeInfoInfo();
+        SvnMergeInfoCatalogInfo catalog = getReposMergeInfoCatalog(repository, relativePath, revision, inheritance, squelchIncapable, false);
+        if (catalog.catalog != null && !catalog.catalog.isEmpty()) {
+            Map<String, SVNMergeRangeList> mi = catalog.catalog.get(catalog.catalog.keySet().iterator().next());
+            result.mergeinfo = mi;
+        }
+        return result;
+    }
+
+    private static SvnMergeInfoCatalogInfo getWcOrReposMergeInfoCatalog(SVNWCContext context, SVNRepository repository, File wcPath, 
+            boolean includeDescendants, boolean reposOnly, boolean ignoreInvalidMergeInfo, SVNMergeInfoInheritance inheritance) throws SVNException {
+        SvnMergeInfoCatalogInfo result = new SvnMergeInfoCatalogInfo();
+        
+        Structure<NodeOriginInfo> nodeOriginInfo = context.getNodeOrigin(wcPath, false, NodeOriginInfo.revision, NodeOriginInfo.reposRelpath, NodeOriginInfo.reposRootUrl);
+        SVNURL url = null;
+        if (nodeOriginInfo.get(NodeOriginInfo.reposRelpath) != null) {
+            url = nodeOriginInfo.get(NodeOriginInfo.reposRootUrl);
+            url = SVNWCUtils.join(url, nodeOriginInfo.<File>get(NodeOriginInfo.reposRelpath));
+        }
+        long revision = nodeOriginInfo.lng(NodeOriginInfo.revision);
+        File reposRelPath = nodeOriginInfo.<File>get(NodeOriginInfo.reposRelpath);
+        nodeOriginInfo.release();
+        
+        Map<String, Map<String, SVNMergeRangeList>> wcMergeInfoCatalog = null;
+        Map<String, Map<String, SVNMergeRangeList>> reposMergeInfoCatalog = null;
+        
+        if (!reposOnly) {
+            SvnMergeInfoCatalogInfo catalogInfo = getWcMergeInfoCatalog(context, includeDescendants, inheritance, wcPath, null, ignoreInvalidMergeInfo);
+            wcMergeInfoCatalog = catalogInfo.catalog;
+            if (catalogInfo.inherited 
+                    || (inheritance == SVNMergeInfoInheritance.EXPLICIT)
+                    || (reposRelPath != null 
+                        && wcMergeInfoCatalog != null
+                        && wcMergeInfoCatalog.get(SVNFileUtil.getFilePath(reposRelPath)) != null)) {
+                reposOnly = true;
+                includeDescendants = false;
+            }
+        }
+        if (reposOnly && url != null) {
+            SVNProperties originalProperties = context.getPristineProps(wcPath);
+            if (!originalProperties.containsName(SVNProperty.MERGE_INFO)) {
+                SVNURL oldLocation = repository.getLocation();
+                try {
+                    repository.setLocation(url, false);
+                    SvnMergeInfoCatalogInfo catalogInfo = getReposMergeInfoCatalog(repository, "", revision, inheritance, true, includeDescendants);
+                    reposMergeInfoCatalog = catalogInfo.catalog;
+                    if (reposMergeInfoCatalog != null && reposMergeInfoCatalog.containsKey(SVNFileUtil.getFilePath(reposRelPath))) {
+                        result.inherited = true;
+                    }
+                } finally {
+                    repository.setLocation(oldLocation, false);
+                }
+                
+            }
+        }
+        if (wcMergeInfoCatalog != null) {
+            result.catalog = wcMergeInfoCatalog;
+            if (reposMergeInfoCatalog != null) {
+                SVNMergeInfoUtil.mergeCatalog(result.catalog, reposMergeInfoCatalog);
+            }
+        } else if (reposMergeInfoCatalog != null) {
+            result.catalog = reposMergeInfoCatalog;
+        }
+        return result;
+    }
+    
+    public static Map<String, Map<String, SVNMergeRangeList>> getMergeInfo(SVNWCContext context, SvnRepositoryAccess repoAccess, SvnTarget target, boolean includeDescendants, boolean ignoreInvalidMergeInfo) throws SVNException {
+        Structure<SvnRepositoryAccess.RepositoryInfo> repositoryInfo = repoAccess.createRepositoryFor(target, SVNRevision.UNDEFINED, target.getPegRevision(), null);
+        SVNURL url = repositoryInfo.get(SvnRepositoryAccess.RepositoryInfo.url);
+        long pegRev = repositoryInfo.lng(SvnRepositoryAccess.RepositoryInfo.revision);
+        long rev = -1;
+        SVNRepository repository = repositoryInfo.get(SvnRepositoryAccess.RepositoryInfo.repository);
+        
+        repositoryInfo.release();
+        boolean useURL = target.isURL();
+        if (!useURL) {
+            SVNURL originURL = null;
+            Structure<NodeOriginInfo> nodeOriginInfo = context.getNodeOrigin(target.getFile(), false, NodeOriginInfo.revision, NodeOriginInfo.reposRelpath, NodeOriginInfo.reposRootUrl);
+            File reposRelPath = nodeOriginInfo.get(NodeOriginInfo.reposRelpath);
+            if (reposRelPath != null) {
+                originURL = nodeOriginInfo.get(NodeOriginInfo.reposRootUrl);
+                originURL = SVNWCUtils.join(originURL, reposRelPath);
+            }
+            rev = nodeOriginInfo.lng(NodeOriginInfo.revision);
+            if (originURL == null 
+                    || !originURL.equals(url)
+                    || pegRev != rev) {
+                useURL = true;
+            }
+            nodeOriginInfo.release();
+        }
+        
+        if (useURL) {
+            rev = pegRev;
+            return getReposMergeInfoCatalog(repository, "", rev, SVNMergeInfoInheritance.INHERITED, false, includeDescendants).catalog;
+        } else {
+            return getWcOrReposMergeInfoCatalog(context, repository, target.getFile(), includeDescendants, false, ignoreInvalidMergeInfo, SVNMergeInfoInheritance.INHERITED).catalog;
+        }
+    }
+ }
