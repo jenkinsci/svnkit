5428f895761700da5dd568e321a44f4daa1f65be
olga.suvorova <>
2012-03-04 20:15:25 +0000

    javadoc for remote delete, remote mk dir

--- svnkit/src/main/java/org/tmatesoft/svn/core/wc2/SvnRemoteDelete.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/wc2/SvnRemoteDelete.java
@@ -1,5 +1,41 @@
 package org.tmatesoft.svn.core.wc2;
 
+import org.tmatesoft.svn.core.SVNPropertyValue;
+import org.tmatesoft.svn.core.wc.ISVNEventHandler;
+import org.tmatesoft.svn.core.wc.SVNEventAction;
+
+/**
+ * Represents delete operation. Deletes items from a repository.
+ * 
+ * <p/>
+ * All operation's targets should be URLs, representing repository locations to be removed. 
+ * URLs can be from mupltiple repositories.
+ * 
+ * <p/>
+ * <code>commitMessage</code> should be set.
+ * 
+ * <p/>
+ * If non-<span class="javakeyword">null</span>,
+ * <code>revisionProperties</code> holds additional, custom revision
+ * properties (<code>String</code> names mapped to {@link SVNPropertyValue}
+ * values) to be set on the new revision. This table cannot contain any
+ * standard Subversion properties.
+ * 
+ * <p/>
+ * {@link #getCommitHandler() Commit handler} will be asked for a commit log
+ * message.
+ * 
+ * <p/>
+ * If the caller's {@link ISVNEventHandler event handler} is not <span
+ * class="javakeyword">null</span> and if the commit succeeds, the handler
+ * will be called with {@link SVNEventAction#COMMIT_COMPLETED} event action.
+ *
+ * <p/>
+ * {@link #run()} method returns {@link SVNCommitInfo} information on a new revision as the result of the commit.
+ * This method throws SVNException if URL does not exist.
+ * 
+ * @author TMate Software Ltd.
+ */
 public class SvnRemoteDelete extends AbstractSvnCommit {
 
     protected SvnRemoteDelete(SvnOperationFactory factory) {
