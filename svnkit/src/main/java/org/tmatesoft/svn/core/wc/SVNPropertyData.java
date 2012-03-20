52cb37b8460d480d4be87355166cbdde088a64fe
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2012-03-20 15:02:03 +0000

    SVNPropertyData: for svn:* properties eols might be replaced with the native ones twice, fixed.

--- svnkit/src/main/java/org/tmatesoft/svn/core/wc/SVNPropertyData.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/wc/SVNPropertyData.java
@@ -56,7 +56,9 @@ public class SVNPropertyData {
         myValue = data;
         if (myValue != null && SVNProperty.isSVNProperty(myName) && myValue.isString()) {
             String nativeEOL = options == null ? System.getProperty("line.separator") : new String(options.getNativeEOL());
-            myValue = SVNPropertyValue.create(myValue.getString().replaceAll("\n", nativeEOL));
+            if (myValue.getString().indexOf(nativeEOL) < 0) {
+                myValue = SVNPropertyValue.create(myValue.getString().replaceAll("\n", nativeEOL));
+            }
         }
     }
     
