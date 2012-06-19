944a1b4c24004fb0c8f48d7dc71e0f258793fb4d
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2012-06-19 15:14:05 +0000

    FSFS: internal auto-unlock option added for commit operation, when enabled, present locks are force-removed on commit.

--- svnkit/src/test/java/org/tmatesoft/svn/test/CommitBuilder.java
+++ svnkit/src/test/java/org/tmatesoft/svn/test/CommitBuilder.java
@@ -1,17 +1,26 @@
 package org.tmatesoft.svn.test;
 
-import org.tmatesoft.svn.core.*;
-import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.tmatesoft.svn.core.SVNCommitInfo;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNNodeKind;
+import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.SVNPropertyValue;
+import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import org.tmatesoft.svn.core.internal.util.SVNPathUtil;
 import org.tmatesoft.svn.core.io.ISVNEditor;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.io.diff.SVNDeltaGenerator;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.*;
 
 public class CommitBuilder {
 
