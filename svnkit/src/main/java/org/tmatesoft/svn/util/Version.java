bf808c197fd1f690d0f512bcbfc25c8dbe639df2
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2012-06-21 16:43:00 +0000

    Version bumped to 1.7.6-SNAPSHOT, CHANGES.txt updated.

--- svnkit/src/main/java/org/tmatesoft/svn/util/Version.java
+++ svnkit/src/main/java/org/tmatesoft/svn/util/Version.java
@@ -33,13 +33,13 @@ public class Version {
     private static final String VERSION_REVISION_PROPERTY = "svnkit.version.revision";
     private static final String SVN_VERSION_PROPERTY = "svnkit.svn.version";  
     
-    private static final String VERSION_STRING_DEFAULT = "SVN/1.7.5 SVNKit/1.7.5 (http://svnkit.com/) rSNAPSHOT";
+    private static final String VERSION_STRING_DEFAULT = "SVN/1.7.6 SVNKit/1.7.6 (http://svnkit.com/) rSNAPSHOT";
     
     private static final String VERSION_MAJOR_DEFAULT = "1";
     private static final String VERSION_MINOR_DEFAULT = "7";
-    private static final String VERSION_MICRO_DEFAULT = "5";
+    private static final String VERSION_MICRO_DEFAULT = "6";
     private static final String VERSION_REVISION_DEFAULT = "SNAPSHOT";
-    private static final String SVN_VERSION_DEFAULT = "1.7.5";
+    private static final String SVN_VERSION_DEFAULT = "1.7.6";
     private static String ourUserAgent;
 
     private static Properties ourProperties;
