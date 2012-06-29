283dae2709427737784e5e38ad8edc1aa7350471
Dmitry Pavlenko <dmit10@gmail.com>
2012-06-29 19:39:47 +0200

    External switch could produce wrong conflicting information in ACTUAL_NODE because base properties weren't read properly.

--- svnkit/src/test/java/org/tmatesoft/svn/test/ExternalsTest.java
+++ svnkit/src/test/java/org/tmatesoft/svn/test/ExternalsTest.java
@@ -70,18 +70,76 @@ public class ExternalsTest {
         }
     }
 
+    @Test
+    public void testExternalTargetChangeDoesntProduceConflictingInformation() throws Exception {
+        final TestOptions options = TestOptions.getInstance();
+
+        final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+        final Sandbox sandbox = Sandbox.createWithCleanup(getTestName() + ".testExternalTargetChangeDoesntProduceConflictingInformation", options);
+        try {
+            final SVNURL url = sandbox.createSvnRepository();
+
+            final SVNExternal external = new SVNExternal("external", url.appendPath("file", false).toString(), SVNRevision.HEAD, SVNRevision.HEAD, false, false, true);
+            final SVNExternal anotherExternal = new SVNExternal("external", url.appendPath("anotherFile", false).toString(), SVNRevision.HEAD, SVNRevision.HEAD, false, false, true);
+
+            final CommitBuilder commitBuilder = new CommitBuilder(url);
+            commitBuilder.addFile("file");
+            commitBuilder.addFile("anotherFile");
+            commitBuilder.setFileProperty("file", "custom property", SVNPropertyValue.create("custom value"));
+            commitBuilder.setDirectoryProperty("", SVNProperty.EXTERNALS, SVNPropertyValue.create(external.toString()));
+            commitBuilder.commit();
+
+            final File workingCopyDirectory = sandbox.createDirectory("wc");
+
+            final SvnCheckout checkout = svnOperationFactory.createCheckout();
+            checkout.setSource(SvnTarget.fromURL(url));
+            checkout.setSingleTarget(SvnTarget.fromFile(workingCopyDirectory));
+            checkout.setIgnoreExternals(false);
+            checkout.run();
+
+            final SvnSetProperty setProperty = svnOperationFactory.createSetProperty();
+            setProperty.setSingleTarget(SvnTarget.fromFile(workingCopyDirectory));
+            setProperty.setPropertyName(SVNProperty.EXTERNALS);
+            setProperty.setPropertyValue(SVNPropertyValue.create(anotherExternal.toString()));
+            setProperty.run();
+
+            final SvnUpdate update = svnOperationFactory.createUpdate();
+            update.setSingleTarget(SvnTarget.fromFile(workingCopyDirectory));
+            update.run();
+
+            final WorkingCopy workingCopy = new WorkingCopy(options, workingCopyDirectory);
+            try {
+                Assert.assertEquals(1, getTableSize(workingCopy, SVNWCDbSchema.ACTUAL_NODE.name()));
+            } finally {
+                workingCopy.dispose();
+            }
+
+        } finally {
+            svnOperationFactory.dispose();
+            sandbox.dispose();
+        }
+    }
+
     private void assertTableIsEmpty(WorkingCopy workingCopy, String tableName) throws SqlJetException {
+        Assert.assertEquals(0, getTableSize(workingCopy, tableName));
+    }
+
+    private int getTableSize(WorkingCopy workingCopy, String tableName) throws SqlJetException {
         final SqlJetDb db = SqlJetDb.open(workingCopy.getWCDbFile(), false);
         try {
             final ISqlJetTable table = db.getTable(tableName);
             db.beginTransaction(SqlJetTransactionMode.READ_ONLY);
             final ISqlJetCursor cursor = table.open();
 
+            int count = 0;
+
             for (; !cursor.eof(); cursor.next()) {
-                Assert.fail("Table " + tableName + " is not empty");
+                count++;
             }
             cursor.close();
             db.commit();
+
+            return count;
         } finally {
             db.close();
         }
