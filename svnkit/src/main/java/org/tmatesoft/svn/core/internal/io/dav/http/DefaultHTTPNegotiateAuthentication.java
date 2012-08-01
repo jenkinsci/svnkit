5500ba3a6d7bbbca471a160d7b41e1158c902090
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2012-08-01 18:25:52 +0000

    Changes made at r9207-9208 reverted (LoginException was always rethrown), exception should not be always rethrown.

--- svnkit/src/main/java/org/tmatesoft/svn/core/internal/io/dav/http/DefaultHTTPNegotiateAuthentication.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/internal/io/dav/http/DefaultHTTPNegotiateAuthentication.java
@@ -139,9 +139,9 @@ public class DefaultHTTPNegotiateAuthentication extends HTTPNegotiateAuthenticat
             SVNDebugLog.getDefaultLog().logFine(SVNLogType.NETWORK, "NEGOTIATE: initialize subject, subject: " + mySubject);
         } catch (LoginException e) {
             SVNDebugLog.getDefaultLog().logFine(SVNLogType.NETWORK, e);
-            SecurityException rethrown = new SecurityException();
-            rethrown.initCause(e);
-            throw rethrown;
+//            SecurityException rethrown = new SecurityException();
+//            rethrown.initCause(e);
+//            throw rethrown;
         }
     }
 
