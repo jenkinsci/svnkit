243662bc0f5fb125909ca06ccb07d98d68ebe959
Dmitry Pavlenko <dmit10@gmail.com>
2012-11-14 14:15:40 +0400

    SVNKIT-326: Added a test that moves a directory with a moved file, works fine.

--- svnkit/src/test/java/org/tmatesoft/svn/test/MoveTest.java
+++ svnkit/src/test/java/org/tmatesoft/svn/test/MoveTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNMoveClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.wc.SVNStatusType;
-import org.tmatesoft.svn.core.wc2.SvnOperationFactory;
-import org.tmatesoft.svn.core.wc2.SvnStatus;
+import org.tmatesoft.svn.core.wc2.*;
 
 import java.io.File;
 import java.util.Map;
 
-@Ignore("SVNKIT-295")
 public class MoveTest {
 
+    @Ignore("SVNKIT-295")
     @Test
     public void testMoveFileOutOfVersionControl() throws Exception {
         //SVNKIT-295
@@ -48,6 +48,61 @@ public class MoveTest {
             } finally {
                 clientManager.dispose();
             }
+        } finally {
+            svnOperationFactory.dispose();
+            sandbox.dispose();
+        }
+    }
+
+    @Test
+    public void testRenamedDirectoryWithMovedFile() throws Exception {
+        final TestOptions options = TestOptions.getInstance();
+
+        final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+        final Sandbox sandbox = Sandbox.createWithCleanup(getTestName() + ".testRenamedDirectoryWithMovedFile", options);
+        try {
+            final SVNURL url = sandbox.createSvnRepository();
+
+            final CommitBuilder commitBuilder = new CommitBuilder(url);
+            commitBuilder.addFile("directory/file");
+            commitBuilder.commit();
+
+            final WorkingCopy workingCopy = sandbox.checkoutNewWorkingCopy(url);
+
+            final File directory = workingCopy.getFile("directory");
+            final File file = new File(directory, "file");
+
+            final File movedDirectory = workingCopy.getFile("movedDirectory");
+            final File movedFile = new File(directory, "movedFile");
+
+            final SvnCopy moveFile = svnOperationFactory.createCopy();
+            moveFile.addCopySource(SvnCopySource.create(SvnTarget.fromFile(file), SVNRevision.WORKING));
+            moveFile.setSingleTarget(SvnTarget.fromFile(movedFile));
+            moveFile.setMove(true);
+            moveFile.run();
+
+            final SvnCopy moveDirectory = svnOperationFactory.createCopy();
+            moveDirectory.addCopySource(SvnCopySource.create(SvnTarget.fromFile(directory), SVNRevision.WORKING));
+            moveDirectory.setSingleTarget(SvnTarget.fromFile(movedDirectory));
+            moveDirectory.setMove(true);
+            moveDirectory.run();
+
+            final Map<File, SvnStatus> statuses = TestUtil.getStatuses(svnOperationFactory, workingCopy.getWorkingCopyDirectory());
+            Assert.assertTrue(statuses.get(movedDirectory).isCopied());
+            Assert.assertNotNull(statuses.get(movedDirectory).getCopyFromUrl());
+
+            Assert.assertTrue(statuses.get(new File(movedDirectory, "movedFile")).isCopied());
+            Assert.assertNotNull(statuses.get(new File(movedDirectory, "movedFile")).getCopyFromUrl());
+
+            Assert.assertTrue(statuses.get(new File(movedDirectory, "file")).isCopied());
+            Assert.assertNull(statuses.get(new File(movedDirectory, "file")).getCopyFromUrl());
+
+            Assert.assertEquals(SVNStatusType.STATUS_ADDED, statuses.get(movedDirectory).getNodeStatus());
+            Assert.assertEquals(SVNStatusType.STATUS_ADDED, statuses.get(new File(movedDirectory, "movedFile")).getNodeStatus());
+            Assert.assertEquals(SVNStatusType.STATUS_DELETED, statuses.get(new File(movedDirectory, "file")).getNodeStatus());
+            Assert.assertEquals(SVNStatusType.STATUS_DELETED, statuses.get(file).getNodeStatus());
+            Assert.assertEquals(SVNStatusType.STATUS_DELETED, statuses.get(directory).getNodeStatus());
+
         } finally {
             svnOperationFactory.dispose();
             sandbox.dispose();
