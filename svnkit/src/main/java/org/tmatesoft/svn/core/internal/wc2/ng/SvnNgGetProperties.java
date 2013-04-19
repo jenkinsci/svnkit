ed3a63c3267d2edaf2e11133c7776fa077e4d47f
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2013-04-19 19:32:47 +0000

    WIP: support for inherited properties, now reported by the SvnGetProperties operation for the working copies.

--- svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgGetProperties.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnNgGetProperties.java
@@ -1,6 +1,8 @@
 package org.tmatesoft.svn.core.internal.wc2.ng;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNErrorCode;
@@ -8,12 +10,21 @@ import org.tmatesoft.svn.core.SVNErrorMessage;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNNodeKind;
 import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.db.SVNSqlJetDb.Mode;
+import org.tmatesoft.svn.core.internal.util.SVNPathUtil;
 import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.internal.wc17.SVNWCContext;
 import org.tmatesoft.svn.core.internal.wc17.db.SVNWCDb;
+import org.tmatesoft.svn.core.internal.wc17.db.SVNWCDb.DirParsedInfo;
+import org.tmatesoft.svn.core.internal.wc17.db.SVNWCDbRoot;
+import org.tmatesoft.svn.core.internal.wc17.db.Structure;
+import org.tmatesoft.svn.core.internal.wc17.db.StructureFields.InheritedProperties;
+import org.tmatesoft.svn.core.internal.wc17.db.SvnWcDbProperties;
 import org.tmatesoft.svn.core.internal.wc2.SvnWcGeneration;
 import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.wc2.SvnGetProperties;
+import org.tmatesoft.svn.core.wc2.SvnInheritedProperties;
 import org.tmatesoft.svn.core.wc2.SvnTarget;
 import org.tmatesoft.svn.util.SVNLogType;
 
@@ -36,6 +47,28 @@ public class SvnNgGetProperties extends SvnNgOperationRunner<SVNProperties, SvnG
         if (kind == SVNNodeKind.UNKNOWN || kind == SVNNodeKind.NONE) {
             SVNErrorMessage err = SVNErrorMessage.create(SVNErrorCode.UNVERSIONED_RESOURCE, "''{0}'' is not under version control", target);
             SVNErrorManager.error(err, SVNLogType.WC);
+        }
+        
+        if (getOperation().getTargetInheritedPropertiesReceiver() != null) {
+            final DirParsedInfo pdh = ((SVNWCDb) context.getDb()).parseDir(target, Mode.ReadOnly);
+            final SVNWCDbRoot wcRoot = pdh.wcDbDir.getWCRoot();
+            final List<Structure<InheritedProperties>> inheritedProps = SvnWcDbProperties.readInheritedProperties(wcRoot, pdh.localRelPath, null);
+            final List<SvnInheritedProperties> resultList = new ArrayList<SvnInheritedProperties>();
+
+            if (inheritedProps != null && !inheritedProps.isEmpty()) {
+                for (Structure<InheritedProperties> props : inheritedProps) {
+                    final SvnInheritedProperties result = new SvnInheritedProperties();
+                    result.setProperties(props.<SVNProperties>get(InheritedProperties.properties));
+                    final String pathOrURL = props.<String>get(InheritedProperties.pathOrURL);
+                    if (SVNPathUtil.isURL(pathOrURL)) {
+                        result.setTarget(SvnTarget.fromURL(SVNURL.parseURIEncoded(pathOrURL)));
+                    } else {
+                        result.setTarget(SvnTarget.fromFile(new File(pathOrURL)));
+                    }
+                    resultList.add(result);
+                }
+                getOperation().getTargetInheritedPropertiesReceiver().receive(getOperation().getFirstTarget(), resultList);
+            }
         }
         
         if (kind == SVNNodeKind.DIR) {
