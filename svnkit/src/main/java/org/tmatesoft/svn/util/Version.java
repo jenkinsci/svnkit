a0b635a106bf8a73c29e82e351bbd400105a9eb0
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2013-07-30 16:11:04 +0000

    Version bumped to 1.7.11-SNAPSHOT, dependencies updated to use Subversion 1.7.11 tests and JavaHL API.

--- svnkit/src/main/java/org/tmatesoft/svn/util/Version.java
+++ svnkit/src/main/java/org/tmatesoft/svn/util/Version.java
@@ -33,11 +33,11 @@ public class Version {
     private static final String VERSION_REVISION_PROPERTY = "svnkit.version.revision";
     private static final String SVN_VERSION_PROPERTY = "svnkit.svn.version";  
     
-    private static final String VERSION_STRING_DEFAULT = "SVN/1.7.9 SVNKit/1.7.10 (http://svnkit.com/) rSNAPSHOT";
+    private static final String VERSION_STRING_DEFAULT = "SVN/1.7.11 SVNKit/1.7.11 (http://svnkit.com/) rSNAPSHOT";
     
     private static final String VERSION_MAJOR_DEFAULT = "1";
     private static final String VERSION_MINOR_DEFAULT = "7";
-    private static final String VERSION_MICRO_DEFAULT = "10";
+    private static final String VERSION_MICRO_DEFAULT = "11";
     private static final String VERSION_REVISION_DEFAULT = "SNAPSHOT";
     private static final String SVN_VERSION_DEFAULT = "1.7.10";
     private static String ourUserAgent;
