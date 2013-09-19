1087b23b70aa28189556dc4a0e5bc7299dad5910
Dmitry Pavlenko <pavlenko@tmatesoft.com>
2013-09-19 13:49:03 +0200

    Fixed compilation for JDK 1.5.

--- svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnCopyAsChangedDiffCallback.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/internal/wc2/ng/SvnCopyAsChangedDiffCallback.java
@@ -14,7 +14,6 @@ public class SvnCopyAsChangedDiffCallback implements ISvnDiffCallback2 {
         this.delegate = delegate;
     }
 
-    @Override
     public void fileOpened(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SvnDiffSource rightSource, SvnDiffSource copyFromSource, boolean createDirBaton, Object dirBaton) throws SVNException {
         if (leftSource == null && copyFromSource != null) {
             assert rightSource != null;
@@ -24,12 +23,10 @@ public class SvnCopyAsChangedDiffCallback implements ISvnDiffCallback2 {
         delegate.fileOpened(result, relPath, leftSource, rightSource, copyFromSource, createDirBaton, dirBaton);
     }
 
-    @Override
     public void fileChanged(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SvnDiffSource rightSource, File leftFile, File rightFile, SVNProperties leftProps, SVNProperties rightProps, boolean fileModified, SVNProperties propChanges) throws SVNException {
         delegate.fileChanged(result, relPath, leftSource, rightSource, leftFile, rightFile, leftProps, rightProps, fileModified, propChanges);
     }
 
-    @Override
     public void fileAdded(SvnDiffCallbackResult result, File relPath, SvnDiffSource copyFromSource, SvnDiffSource rightSource, File copyFromFile, File rightFile, SVNProperties copyFromProps, SVNProperties rightProps) throws SVNException {
         if (copyFromSource != null) {
             SVNProperties propChanges = rightProps.compareTo(copyFromProps);
@@ -47,17 +44,14 @@ public class SvnCopyAsChangedDiffCallback implements ISvnDiffCallback2 {
         }
     }
 
-    @Override
     public void fileDeleted(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, File leftFile, SVNProperties leftProps) throws SVNException {
         delegate.fileDeleted(result, relPath, leftSource, leftFile, leftProps);
     }
 
-    @Override
     public void fileClosed(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SvnDiffSource rightSource) throws SVNException {
         delegate.fileClosed(result, relPath, leftSource, rightSource);
     }
 
-    @Override
     public void dirOpened(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SvnDiffSource rightSource, SvnDiffSource copyFromSource, Object dirBaton) throws SVNException {
         if (leftSource == null && copyFromSource != null) {
             assert rightSource != null;
@@ -67,17 +61,14 @@ public class SvnCopyAsChangedDiffCallback implements ISvnDiffCallback2 {
         delegate.dirOpened(result, relPath, leftSource, rightSource, copyFromSource, dirBaton);
     }
 
-    @Override
     public void dirChanged(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SvnDiffSource rightSource, SVNProperties leftProps, SVNProperties rightProps, SVNProperties propChanges, Object dirBaton) throws SVNException {
         delegate.dirChanged(result, relPath, leftSource, rightSource, leftProps, rightProps, propChanges, dirBaton);
     }
 
-    @Override
     public void dirDeleted(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SVNProperties leftProps, Object dirBaton) throws SVNException {
         delegate.dirDeleted(result, relPath, leftSource, leftProps, dirBaton);
     }
 
-    @Override
     public void dirAdded(SvnDiffCallbackResult result, File relPath, SvnDiffSource copyFromSource, SvnDiffSource rightSource, SVNProperties copyFromProps, SVNProperties rightProps, Object dirBaton) throws SVNException {
         if (copyFromSource != null) {
             SVNProperties propChanges = rightProps.compareTo(copyFromProps);
@@ -87,12 +78,10 @@ public class SvnCopyAsChangedDiffCallback implements ISvnDiffCallback2 {
         }
     }
 
-    @Override
     public void dirClosed(SvnDiffCallbackResult result, File relPath, SvnDiffSource leftSource, SvnDiffSource rightSource, Object dirBaton) throws SVNException {
         delegate.dirClosed(result, relPath, leftSource, rightSource, dirBaton);
     }
 
-    @Override
     public void nodeAbsent(SvnDiffCallbackResult result, File relPath, Object dirBaton) throws SVNException {
         delegate.nodeAbsent(result, relPath, dirBaton);
     }
